2129457dc66c79d2d3d9a350f9ce9d8e850521f5
yuuki1293 <yuuki1293@outlook.jp>
2022-09-04 01:06:14 +0900

    optimize imports

--- src/main/scala/com/yuuki1293/bookbook/BookBook.java
+++ src/main/scala/com/yuuki1293/bookbook/BookBook.java
@@ -1,18 +1,11 @@
 package com.yuuki1293.bookbook;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(BookBook.MODID)
