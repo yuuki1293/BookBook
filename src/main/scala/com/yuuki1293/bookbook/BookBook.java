c783a8ade26714316293c4cb2db26a25ecea68b3
yuuki1293 <yuuki1293@outlook.jp>
2022-09-06 14:56:03 +0900

    本を投げたときにCustomNameが変わる

--- src/main/scala/com/yuuki1293/bookbook/BookBook.java
+++ src/main/scala/com/yuuki1293/bookbook/BookBook.java
@@ -1,18 +1,15 @@
 package com.yuuki1293.bookbook;
 
-import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(BookBook.MODID)
 public class BookBook {
     public static final String MODID = "bookbook";
     // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
 
     public BookBook() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
@@ -21,5 +18,6 @@ public class BookBook {
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
+        MinecraftForge.EVENT_BUS.register(new Main());
     }
 }
--- src/main/scala/com/yuuki1293/bookbook/Main.scala
+++ src/main/scala/com/yuuki1293/bookbook/Main.scala
@@ -1,5 +1,22 @@
 package com.yuuki1293.bookbook
 
-object Main {
+import net.minecraft.nbt._
+import net.minecraft.network.chat.TextComponent
+import net.minecraftforge.event.entity.item.ItemTossEvent
+import net.minecraftforge.eventbus.api.SubscribeEvent
+import net.minecraftforge.fml.common.Mod
 
+@Mod.EventBusSubscriber
+class Main {
+  @SubscribeEvent
+  def drawingBook(event: ItemTossEvent): Unit = {
+    val item = event.getEntityItem
+    val name = StringTag.valueOf("[\"\",{\"text\":\"b\",\"obfuscated\":true,\"color\":\"black\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_blue\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_green\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_aqua\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_red\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_purple\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"gold\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"gray\"},{\"text\":\"\\u672c\\u304c\\u6eba\\u308c\\u305f\\uff8c\\uff9e\\uff6f\\uff78\\uff8c\\uff9e\\uff6f\\uff78\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_gray\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"blue\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"green\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"aqua\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"red\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"light_purple\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"yellow\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"white\"}]")
+
+    item.setCustomNameVisible(true)
+
+    val nbt = item.serializeNBT().copy()
+    nbt.put("CustomName", name)
+    item.deserializeNBT(nbt)
+  }
 }
