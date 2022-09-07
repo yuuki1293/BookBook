5162611d167c150aceaeff3fda20ff335413ab8e
yuuki1293 <yuuki1293@outlook.jp>
2022-09-07 18:32:13 +0900

    update: RegisterEntity

---
+++ src/main/scala/com/yuuki1293/bookbook/common/register/RegisterEntity.scala
@@ -0,0 +1,7 @@
+package com.yuuki1293.bookbook.common.register
+
+import net.minecraftforge.registries.{DeferredRegister, ForgeRegistries}
+
+object RegisterEntity {
+  val ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITIES, "bookbook")
+}
