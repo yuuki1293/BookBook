9f2c5892242b9b7bb6fb35e6491e84f6b8dfdae6
yuuki1293 <yuuki1293@outlook.jp>
2022-09-07 18:48:40 +0900

    update: RegisterItem

---
+++ src/main/scala/com/yuuki1293/bookbook/common/register/RegisterItem.scala
@@ -0,0 +1,16 @@
+package com.yuuki1293.bookbook.common.register
+
+import com.yuuki1293.bookbook.common.item.ItemBook
+import net.minecraft.world.item.{CreativeModeTab, Item}
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
+import net.minecraftforge.registries.{DeferredRegister, ForgeRegistries, RegistryObject}
+
+object RegisterItem {
+  val ITEMS: DeferredRegister[Item] = DeferredRegister.create(ForgeRegistries.ITEMS, "bookbook")
+
+  val BOOK: RegistryObject[ItemBook] = ITEMS.register("book", () => new ItemBook(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)))
+
+  def registry(): Unit = {
+    ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus)
+  }
+}
