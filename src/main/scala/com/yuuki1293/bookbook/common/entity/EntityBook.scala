3c5557cc421d48007cb4485adbfd9f5a9a13bdab
yuuki1293 <yuuki1293@outlook.jp>
2022-09-10 09:33:16 +0900

    いろいろ試し中

--- src/main/scala/com/yuuki1293/bookbook/common/entity/EntityBook.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/entity/EntityBook.scala
@@ -1,19 +1,23 @@
 package com.yuuki1293.bookbook.common.entity
 
+import com.yuuki1293.bookbook.common.BookBook
 import net.minecraft.nbt.StringTag
 import net.minecraft.world.entity.item.ItemEntity
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
+import org.apache.logging.log4j.{LogManager, Logger}
 
 class EntityBook(pLevel: Level, pPosX: Double, pPosY: Double, pPosZ: Double, pItemStack: ItemStack)
   extends ItemEntity(pLevel, pPosX, pPosY, pPosZ, pItemStack) {
+
+  val LOGGER: Logger = LogManager.getLogger(BookBook.MODID)
 
   this.setPos(pPosX, pPosY, pPosZ)
   this.setItem(pItemStack)
 
   override def tick(): Unit = {
     super.tick()
-
+    LOGGER.debug("ﾃｨｯｸﾃｨｯｸ")
     if (this.isInWater) {
       val name = StringTag.valueOf("[\"\",{\"text\":\"b\",\"obfuscated\":true,\"color\":\"black\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_blue\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_green\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_aqua\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_red\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_purple\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"gold\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"gray\"},{\"text\":\"\\u672c\\u304c\\u6eba\\u308c\\u305f\\uff8c\\uff9e\\uff6f\\uff78\\uff8c\\uff9e\\uff6f\\uff78\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_gray\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"blue\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"green\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"aqua\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"red\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"light_purple\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"yellow\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"white\"}]")
 
--- src/main/scala/com/yuuki1293/bookbook/common/item/ItemBook.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/item/ItemBook.scala
@@ -1,7 +1,18 @@
 package com.yuuki1293.bookbook.common.item
 
-import net.minecraft.world.item.Item
+import com.yuuki1293.bookbook.common.entity.EntityBook
+import net.minecraft.world.entity.Entity
+import net.minecraft.world.item.{Item, ItemStack}
+import net.minecraft.world.level.Level
 
 class ItemBook(pProperties: Item.Properties) extends Item(pProperties) {
+  override def createEntity(level: Level, location: Entity, stack: ItemStack): Entity = {
+    val entity = new EntityBook(level, location.getX, location.getY, location.getZ, stack)
 
+    entity.setDeltaMovement(location.getDeltaMovement)
+
+    entity.setPickUpDelay(40)
+
+    entity
+  }
 }
