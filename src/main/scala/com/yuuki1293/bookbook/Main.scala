a658a86f4c3e0516101f0ad0ed63317337de5a3e
yuuki1293 <yuuki1293@outlook.jp>
2022-09-12 18:39:38 +0900

    delete: Main.scala

--- src/main/scala/com/yuuki1293/bookbook/Main.scala
+++
@@ -1,9 +0,0 @@
-package com.yuuki1293.bookbook
-
-import net.minecraftforge.fml.common.Mod
-import org.apache.logging.log4j.{LogManager, Logger}
-
-@Mod.EventBusSubscriber
-class Main {
-  val LOGGER: Logger = LogManager.getLogger("bookbook")
-}
