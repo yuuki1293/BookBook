e6fecc4750369a7a5191a176372238119357c0c2
yuuki1293 <yuuki1293@outlook.jp>
2022-09-15 20:17:43 +0900

    update: EntityItemBook

--- src/main/scala/com/yuuki1293/bookbook/common/entity/EntityItemBook.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/entity/EntityItemBook.scala
@@ -9,6 +9,7 @@ import net.minecraft.world.level.Level
 import net.minecraftforge.network.NetworkHooks
 
 class EntityItemBook(entityType: EntityType[_ <: ItemEntity], level: Level) extends ItemEntity(entityType, level) {
+  setDrawn(false)
 
   def this(entityType: EntityType[_ <: ItemEntity], level: Level, posX: Double, posY: Double, posZ: Double, itemStack: ItemStack) = {
     this(entityType, level)
@@ -25,9 +26,21 @@ class EntityItemBook(entityType: EntityType[_ <: ItemEntity], level: Level) exte
     super.tick()
 
     if (this.isInWater) {
-      val name = new TextComponent("[\"\",{\"text\":\"b\",\"obfuscated\":true,\"color\":\"black\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_blue\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_green\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_aqua\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_red\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_purple\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"gold\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"gray\"},{\"text\":\"\\u672c\\u304c\\u6eba\\u308c\\u305f\\uff8c\\uff9e\\uff6f\\uff78\\uff8c\\uff9e\\uff6f\\uff78\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_gray\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"blue\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"green\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"aqua\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"red\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"light_purple\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"yellow\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"white\"}]")
-      this.setCustomName(name)
-      this.setCustomNameVisible(true)
+      setDrawn(true)
     }
+  }
+
+  def isDrawn(): Boolean = {
+    val nbt = this.serializeNBT()
+
+    nbt.getBoolean("drawn")
+  }
+
+  def setDrawn(drawn: Boolean): Unit ={
+    val nbt = this.serializeNBT()
+
+    nbt.putBoolean("drawn", drawn)
+
+    this.deserializeNBT(nbt)
   }
 }
