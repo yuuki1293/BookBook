5c01dc0102498b0429d22c7838b4da3b5b1a5585
yuuki1293 <yuuki1293@outlook.jp>
2022-09-15 19:46:49 +0900

    delete: CommonEvent

--- src/main/scala/com/yuuki1293/bookbook/common/register/RegisterEvent.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/register/RegisterEvent.scala
@@ -1,10 +1,6 @@
 package com.yuuki1293.bookbook.common.register
 
-import com.yuuki1293.bookbook.common.event.CommonEvent
-import net.minecraftforge.common.MinecraftForge
-
 object RegisterEvent {
   def registry(): Unit ={
-    MinecraftForge.EVENT_BUS.register(new CommonEvent())
   }
 }
