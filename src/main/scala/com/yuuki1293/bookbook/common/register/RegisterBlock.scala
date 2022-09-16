2fec411b49c2fc30ff8123b473daa19a5013adbc
yuuki1293 <yuuki1293@outlook.jp>
2022-09-16 16:53:02 +0900

    rename bookshelf

--- src/main/scala/com/yuuki1293/bookbook/common/register/RegisterBlock.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/register/RegisterBlock.scala
@@ -2,14 +2,16 @@ package com.yuuki1293.bookbook.common.register
 
 import com.yuuki1293.bookbook.common.BookBook
 import com.yuuki1293.bookbook.common.block.BlockBookShelf
-import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.{Block, SoundType}
+import net.minecraft.world.level.block.state.BlockBehaviour
+import net.minecraft.world.level.material.Material
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
 import net.minecraftforge.registries.{DeferredRegister, ForgeRegistries, RegistryObject}
 
 object RegisterBlock {
   val BLOCKS: DeferredRegister[Block] = DeferredRegister.create(ForgeRegistries.BLOCKS, BookBook.MODID)
 
-  val BOOK_SHELF: RegistryObject[BlockBookShelf] = BLOCKS.register("book_shelf", () => new BlockBookShelf())
+  val BOOK_SHELF: RegistryObject[BlockBookShelf] = BLOCKS.register("bookshelf", () => new BlockBookShelf(BlockBehaviour.Properties.of(Material.WOOD).strength(1.5F).sound(SoundType.WOOD)))
 
   def registry(): Unit = {
     BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus)
