d1100f331aa94cc436ad991bb8c74c31ed5f6b61
yuuki1293 <yuuki1293@outlook.jp>
2022-09-16 16:29:24 +0900

    update: RegisterBlock

--- src/main/scala/com/yuuki1293/bookbook/common/register/RegisterBlock.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/register/RegisterBlock.scala
@@ -1,5 +1,17 @@
 package com.yuuki1293.bookbook.common.register
 
-object RegisterBlock {
+import com.yuuki1293.bookbook.common.BookBook
+import com.yuuki1293.bookbook.common.block.BlockBookShelf
+import net.minecraft.world.level.block.Block
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
+import net.minecraftforge.registries.{DeferredRegister, ForgeRegistries, RegistryObject}
 
+object RegisterBlock {
+  val BLOCKS: DeferredRegister[Block] = DeferredRegister.create(ForgeRegistries.BLOCKS, BookBook.MODID)
+
+  val BOOK_SHELF: RegistryObject[BlockBookShelf] = BLOCKS.register("book_shelf", () => new BlockBookShelf())
+
+  def registry(): Unit = {
+    BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus)
+  }
 }
