480bcf205b13388bcde8bec0828dcfd2d077b80b
yuuki1293 <yuuki1293@outlook.jp>
2022-09-16 16:29:31 +0900

    update: RegisterItem

--- src/main/scala/com/yuuki1293/bookbook/common/register/RegisterItem.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/register/RegisterItem.scala
@@ -1,12 +1,13 @@
 package com.yuuki1293.bookbook.common.register
 
+import com.yuuki1293.bookbook.common.BookBook
 import com.yuuki1293.bookbook.common.item.{ItemBook, ItemDrownedBook}
 import net.minecraft.world.item.Item
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
 import net.minecraftforge.registries.{DeferredRegister, ForgeRegistries, RegistryObject}
 
 object RegisterItem {
-  val ITEMS: DeferredRegister[Item] = DeferredRegister.create(ForgeRegistries.ITEMS, "bookbook")
+  val ITEMS: DeferredRegister[Item] = DeferredRegister.create(ForgeRegistries.ITEMS, BookBook.MODID)
 
   val BOOK: RegistryObject[ItemBook] = ITEMS.register("book", () => new ItemBook)
   val DROWNED_BOOK: RegistryObject[ItemDrownedBook] = ITEMS.register("drowned_book", () => new ItemDrownedBook)
