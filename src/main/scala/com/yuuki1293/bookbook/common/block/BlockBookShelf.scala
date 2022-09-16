ea324117362359eabc1000f3454280734f343852
yuuki1293 <yuuki1293@outlook.jp>
2022-09-16 16:53:44 +0900

    delete: unused constructor

--- src/main/scala/com/yuuki1293/bookbook/common/block/BlockBookShelf.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/block/BlockBookShelf.scala
@@ -3,11 +3,9 @@ package com.yuuki1293.bookbook.common.block
 import net.minecraft.core.BlockPos
 import net.minecraft.world.level.LevelReader
 import net.minecraft.world.level.block.Block
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties
+import net.minecraft.world.level.block.state.BlockBehaviour
 import net.minecraft.world.level.block.state.BlockState
 
-class BlockBookShelf(properties: Properties) extends Block(properties) {
-  def this() = this(new Properties())
-
+class BlockBookShelf(properties: BlockBehaviour.Properties) extends Block(properties) {
   override def getEnchantPowerBonus(state: BlockState, level: LevelReader, pos: BlockPos): Float = 1
 }
