3603aad0087e70bf227d00769ce463f5942a454a
yuuki1293 <yuuki1293@outlook.jp>
2022-09-19 22:21:16 +0900

    update: BlockBookShelf

--- src/main/scala/com/yuuki1293/bookbook/common/block/BlockBookShelf.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/block/BlockBookShelf.scala
@@ -3,8 +3,7 @@ package com.yuuki1293.bookbook.common.block
 import net.minecraft.core.BlockPos
 import net.minecraft.world.level.LevelReader
 import net.minecraft.world.level.block.Block
-import net.minecraft.world.level.block.state.BlockBehaviour
-import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.{BlockBehaviour, BlockState}
 
 class BlockBookShelf(properties: BlockBehaviour.Properties) extends Block(properties) {
   override def getEnchantPowerBonus(state: BlockState, level: LevelReader, pos: BlockPos): Float = 1
