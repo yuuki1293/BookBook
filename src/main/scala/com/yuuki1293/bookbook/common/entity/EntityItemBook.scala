6c8abf26b5f2aa5cbdede13cf0b223b7199179f5
yuuki1293 <yuuki1293@outlook.jp>
2022-09-20 11:10:40 +0900

    fix: tagが違った

--- src/main/scala/com/yuuki1293/bookbook/common/entity/EntityItemBook.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/entity/EntityItemBook.scala
@@ -37,8 +37,11 @@ class EntityItemBook(entityType: EntityType[_ <: ItemEntity], level: Level) exte
 
       val name = StringTag.valueOf("[\"\",{\"text\":\"b\",\"obfuscated\":true,\"color\":\"black\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_blue\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_green\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_aqua\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_red\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_purple\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"gold\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"gray\"},{\"text\":\"\\u672c\\u304c\\u6eba\\u308c\\u305f\\uff8c\\uff9e\\uff6f\\uff78\\uff8c\\uff9e\\uff6f\\uff78\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"dark_gray\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"blue\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"green\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"aqua\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"red\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"light_purple\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"yellow\"},{\"text\":\"b\",\"obfuscated\":true,\"color\":\"white\"}]")
 
-      this.getItem.getOrCreateTag().put("CustomName", name)
       this.setCustomNameVisible(true)
+
+      val nbt = this.serializeNBT().copy()
+      nbt.put("CustomName", name)
+      this.deserializeNBT(nbt)
     }
   }
 }
