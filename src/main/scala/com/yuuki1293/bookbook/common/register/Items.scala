1632da63b62bebca081c69173d3e6a3c9e9997f3
yuuki1293 <yuuki1293@outlook.jp>
2022-09-20 21:53:49 +0900

    fix: error

--- src/main/scala/com/yuuki1293/bookbook/common/register/Items.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/register/Items.scala
@@ -1,29 +1,30 @@
 package com.yuuki1293.bookbook.common.register
 
 import com.yuuki1293.bookbook.common.BookBook
-import com.yuuki1293.bookbook.common.item.{ItemBook, BaseItemCompressed, ItemDrownedBook}
-import net.minecraft.world.item.Item.Properties
+import com.yuuki1293.bookbook.common.item.{BaseItemCompressed, ItemBook, ItemDrownedBook}
 import net.minecraft.world.item.{BlockItem, CreativeModeTab, Item}
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
 import net.minecraftforge.registries.{DeferredRegister, ForgeRegistries, RegistryObject}
 
+import java.util.function.Supplier
+
 object Items {
   val ITEMS: DeferredRegister[Item] = DeferredRegister.create(ForgeRegistries.ITEMS, BookBook.MODID)
 
-  val BOOK: RegistryObject[ItemBook] = ITEMS.register("book", () => new ItemBook(new Properties().tab(CreativeModeTab.TAB_MATERIALS)))
-  val DROWNED_BOOK: RegistryObject[ItemDrownedBook] = ITEMS.register("drowned_book", () => new ItemDrownedBook(new Properties().tab(CreativeModeTab.TAB_MATERIALS)))
-  val COMPRESSED_BOOK_0: RegistryObject[BaseItemCompressed] = registryCBook(0, new Properties().tab(CreativeModeTab.TAB_MATERIALS))
-  val COMPRESSED_BOOK_1: RegistryObject[BaseItemCompressed] = registryCBook(1, new Properties().tab(CreativeModeTab.TAB_MATERIALS))
-  val COMPRESSED_BOOK_2: RegistryObject[BaseItemCompressed] = registryCBook(2, new Properties().tab(CreativeModeTab.TAB_MATERIALS))
+  val BOOK: RegistryObject[ItemBook] = ITEMS.register("book", () => new ItemBook(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)))
+  val DROWNED_BOOK: RegistryObject[ItemDrownedBook] = ITEMS.register("drowned_book", () => new ItemDrownedBook(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)))
+  val COMPRESSED_BOOK_0: RegistryObject[BaseItemCompressed] = registryCBook(0, new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS))
+  val COMPRESSED_BOOK_1: RegistryObject[BaseItemCompressed] = registryCBook(1, new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS))
+  val COMPRESSED_BOOK_2: RegistryObject[BaseItemCompressed] = registryCBook(2, new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS))
 
-  val BOOKSHELF: RegistryObject[BlockItem] = ITEMS.register(Blocks.BOOKSHELF.getId.getPath, () => new BlockItem(Blocks.BOOKSHELF.get(), new Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)))
-  val DROWNED_BOOKSHELF: RegistryObject[BlockItem] = ITEMS.register(Blocks.DROWNED_BOOKSHELF.getId.getPath, () => new BlockItem(Blocks.DROWNED_BOOKSHELF.get(), new Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)))
+  val BOOKSHELF: RegistryObject[BlockItem] = ITEMS.register(Blocks.BOOKSHELF.getId.getPath, () => new BlockItem(Blocks.BOOKSHELF.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)))
+  val DROWNED_BOOKSHELF: RegistryObject[BlockItem] = ITEMS.register(Blocks.DROWNED_BOOKSHELF.getId.getPath, () => new BlockItem(Blocks.DROWNED_BOOKSHELF.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)))
 
-  def registryCompressed[T <: Item](prefix: String)(tier: Int, properties: Properties): RegistryObject[T] = {
-    ITEMS.register(s"$prefix$tier", () => new T(properties))
+  def registryCompressed[T <: Item](prefix: String)(tier: Int, properties: Item.Properties)(implicit tag : reflect.ClassTag[T]): RegistryObject[T] = {
+    ITEMS.register(s"$prefix$tier", () => tag.runtimeClass.getDeclaredConstructor(classOf[Item.Properties]).newInstance(properties).asInstanceOf[T])
   }
 
-  def registryCBook: (Int, Properties) => RegistryObject[BaseItemCompressed] = registryCompressed[BaseItemCompressed]("compressed_book_")
+  def registryCBook: (Int, Item.Properties) => RegistryObject[BaseItemCompressed] = registryCompressed[BaseItemCompressed]("compressed_book_")
 
   def registry(): Unit = {
     ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus)
