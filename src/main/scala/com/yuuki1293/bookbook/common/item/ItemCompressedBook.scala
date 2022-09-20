d53965b68cf557d3714ebe4c97de9c1f1503dd86
yuuki1293 <yuuki1293@outlook.jp>
2022-09-20 19:31:29 +0900

    add: ItemCompressedBook

---
+++ src/main/scala/com/yuuki1293/bookbook/common/item/ItemCompressedBook.scala
@@ -0,0 +1,8 @@
+package com.yuuki1293.bookbook.common.item
+
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.Item.Properties
+
+class ItemCompressedBook(properties: Properties) extends Item(properties){
+  
+}
