9b2100c1b0c4ea4945f593549c79cba6016cc01c
yuuki1293 <yuuki1293@outlook.jp>
2022-09-24 13:08:29 +0900

    fix: #3

--- src/main/scala/com/yuuki1293/bookbook/common/block/BlockBookFurnace.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/block/BlockBookFurnace.scala
@@ -33,22 +33,21 @@ class BlockBookFurnace(properties: BlockBehaviour.Properties) extends AbstractFu
     }
   }
 
-  override def animateTick(pState: BlockState, pLevel: Level, pPos: BlockPos, pRandom: Random): Unit = {
+  override def animateTick(pState: BlockState, pLevel: Level, pPos: BlockPos, pRand: Random): Unit = {
     if (pState.getValue(LIT)) {
-      val d0: Double = pPos.getX + 0.5D
-      val d1: Double = pPos.getY
-      val d2: Double = pPos.getZ + 0.5D
-      if (pRandom.nextDouble() < 0.1D) {
+      val d0 = pPos.getX.toDouble + 0.5D
+      val d1 = pPos.getY.toDouble
+      val d2 = pPos.getZ.toDouble + 0.5D
+      if (pRand.nextDouble < 0.1D)
         pLevel.playLocalSound(d0, d1, d2, SoundEvents.FURNACE_FIRE_CRACKLE, SoundSource.BLOCKS, 1.0F, 1.0F, false)
-      }
 
       val direction = pState.getValue(FACING)
       val direction$axis = direction.getAxis
-      val d3: Double = 0.52D
-      val d4: Double = pRandom.nextDouble() * 0.6D - 0.3D
-      val d5: Double = if (direction$axis == Direction.Axis.X) direction.getStepX.toDouble * 0.5D else d4
-      val d6: Double = pRandom.nextDouble() * 6.0D / 16.0D
-      val d7: Double = if (direction$axis == Direction.Axis.Z) direction.getStepX.toDouble * 0.5D else d4
+      val d3 = 0.52D
+      val d4 = pRand.nextDouble * 0.6D - 0.3D
+      val d5 = if (direction$axis eq Direction.Axis.X) direction.getStepX.toDouble * 0.52D else d4
+      val d6 = pRand.nextDouble * 6.0D / 16.0D
+      val d7 = if (direction$axis eq Direction.Axis.Z) direction.getStepZ.toDouble * 0.52D else d4
       pLevel.addParticle(ParticleTypes.SMOKE, d0 + d5, d1 + d6, d2 + d7, 0.0D, 0.0D, 0.0D)
       pLevel.addParticle(ParticleTypes.FLAME, d0 + d5, d1 + d6, d2 + d7, 0.0D, 0.0D, 0.0D)
     }
