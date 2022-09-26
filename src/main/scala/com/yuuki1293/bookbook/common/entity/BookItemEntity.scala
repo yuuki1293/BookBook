3e60addcb37cd17bf1542c28a5fcb6767da7c2d8
yuuki1293 <yuuki1293@outlook.jp>
2022-09-26 10:23:18 +0900

    rename: BookItemEntity

--- src/main/scala/com/yuuki1293/bookbook/common/entity/BookItemEntity.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/entity/BookItemEntity.scala
@@ -9,7 +9,7 @@ import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
 import net.minecraftforge.network.NetworkHooks
 
-class ItemBookEntity(entityType: EntityType[_ <: ItemEntity], level: Level) extends ItemEntity(entityType, level) {
+class BookItemEntity(entityType: EntityType[_ <: ItemEntity], level: Level) extends ItemEntity(entityType, level) {
   def this(entityType: EntityType[_ <: ItemEntity], level: Level, posX: Double, posY: Double, posZ: Double, itemStack: ItemStack) = {
     this(entityType, level)
     this.setPos(posX, posY, posZ)
