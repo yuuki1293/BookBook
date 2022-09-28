64fd047a026dbeec96793d6436a8b00780eae5dd
yuuki1293 <yuuki1293@outlook.jp>
2022-09-28 10:37:06 +0900

    CustomRecipeは諦める

--- src/main/scala/com/yuuki1293/bookbook/common/recipe/custom/WaterproofedBookRecipe.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/recipe/custom/WaterproofedBookRecipe.scala
@@ -1,94 +1,69 @@
 package com.yuuki1293.bookbook.common.recipe.custom
 
+import com.google.common.collect.Lists
 import com.yuuki1293.bookbook.common.item.BookItem
 import com.yuuki1293.bookbook.common.register
+import com.yuuki1293.bookbook.common.register.Recipe
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.inventory.CraftingContainer
-import net.minecraft.world.item
-import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.crafting.{CustomRecipe, RecipeSerializer, SimpleRecipeSerializer}
+import net.minecraft.world.item.crafting.{CustomRecipe, RecipeSerializer}
+import net.minecraft.world.item.{DyeItem, Item, ItemStack}
 import net.minecraft.world.level.Level
 
-class WaterproofedBookRecipe(pId: ResourceLocation) extends CustomRecipe(pId){
+import java.util
+
+class WaterproofedBookRecipe(pId: ResourceLocation) extends CustomRecipe(pId) {
   override def matches(pInv: CraftingContainer, pLevel: Level): Boolean = {
-    var book = false
-    var orangeDye = false
-
+    var itemstack: ItemStack = ItemStack.EMPTY
+    val list: util.List[ItemStack] = Lists.newArrayList
     for (i <- 0 until pInv.getContainerSize) {
-      val itemStack1 = pInv.getItem(i)
-      if (!itemStack1.isEmpty) {
-        if (itemStack1.is(register.Items.BOOK.get())) {
-          if(book){
-            return false
-          }
-
-          book = true
-        } else if (!itemStack1.is(item.Items.ORANGE_DYE)) {
-          if (orangeDye){
-            return false
-          }
-
-          orangeDye = true
-        }
+      val itemstack1: ItemStack = pInv.getItem(i)
+      if (!itemstack1.isEmpty) if (itemstack1.getItem.isInstanceOf[BookItem]) {
+        if (!itemstack.isEmpty) return false
+        itemstack = itemstack1
+      }
+      else {
+        if (!itemstack1.getItem.isInstanceOf[DyeItem]) return false
+        list.add(itemstack1)
       }
     }
-
-    book && orangeDye
+    !itemstack.isEmpty && !list.isEmpty
   }
 
+  /**
+   * Returns an Item that is the result of this recipe
+   */
   override def assemble(pInv: CraftingContainer): ItemStack = {
-    var book = false
-    var orangeDye = false
-
+    val list: util.List[DyeItem] = Lists.newArrayList
+    var itemstack: ItemStack = ItemStack.EMPTY
     for (i <- 0 until pInv.getContainerSize) {
-      val itemStack1 = pInv.getItem(i)
-      if (!itemStack1.isEmpty) {
-        if (itemStack1.is(register.Items.BOOK.get())) {
-          if (book) {
-            return ItemStack.EMPTY
-          }
-
-          book = true
-        } else if (!itemStack1.is(item.Items.ORANGE_DYE)) {
-          if (orangeDye) {
-            return ItemStack.EMPTY
-          }
-
-          orangeDye = true
+      val itemstack1: ItemStack = pInv.getItem(i)
+      if (!itemstack1.isEmpty) {
+        val item: Item = itemstack1.getItem
+        if (item.isInstanceOf[BookItem]) {
+          if (!itemstack.isEmpty) return ItemStack.EMPTY
+          itemstack = itemstack1.copy
+        }
+        else {
+          if (!item.isInstanceOf[DyeItem]) return ItemStack.EMPTY
+          list.add(item.asInstanceOf[DyeItem])
         }
       }
     }
-
-    if (book && orangeDye) getResultItem else ItemStack.EMPTY
+    if (!itemstack.isEmpty && !list.isEmpty) {
+      getResultItem
+    } else ItemStack.EMPTY
   }
-
-//  override def getRemainingItems(pContainer: CraftingContainer): NonNullList[ItemStack] = {
-//    NonNullList.withSize(pContainer.getContainerSize, ItemStack.EMPTY)
-//  }
-
-  override def getSerializer: RecipeSerializer[WaterproofedBookRecipe] = WaterproofedBookRecipe.getSerializer
-
-  override def canCraftInDimensions(pWidth: Int, pHeight: Int): Boolean = pWidth * pHeight >= 2
-
-  override def isSpecial: Boolean = false
 
   override def getResultItem: ItemStack = {
     val itemStack = new ItemStack(register.Items.BOOK.get())
     BookItem.setWaterProof(itemStack, p = true)
     itemStack
   }
-}
 
-object WaterproofedBookRecipe {
-  var SERIALIZER: Option[SimpleRecipeSerializer[WaterproofedBookRecipe]] = None
+  override def canCraftInDimensions(pWidth: Int, pHeight: Int): Boolean = pWidth * pHeight >= 2
 
-  def getSerializer: RecipeSerializer[WaterproofedBookRecipe] = {
-    SERIALIZER match {
-      case Some(value) => value
-      case None =>
-        val serializer = new SimpleRecipeSerializer(new WaterproofedBookRecipe(_))
-        SERIALIZER = Some(serializer)
-        serializer
-    }
-  }
+  override def getSerializer: RecipeSerializer[_] = Recipe.WATERPROOFED_BOOK.get()
+
+  override def isSpecial: Boolean = false
 }
--- src/main/scala/com/yuuki1293/bookbook/common/register/Recipe.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/register/Recipe.scala
@@ -3,7 +3,7 @@ package com.yuuki1293.bookbook.common.register
 import com.yuuki1293.bookbook.common.BookBook
 import com.yuuki1293.bookbook.common.recipe.custom.WaterproofedBookRecipe
 import net.minecraft.core.Registry
-import net.minecraft.world.item.crafting.RecipeSerializer
+import net.minecraft.world.item.crafting.{RecipeSerializer, SimpleRecipeSerializer}
 import net.minecraftforge.eventbus.api.IEventBus
 import net.minecraftforge.registries.{DeferredRegister, RegistryObject}
 
@@ -11,7 +11,7 @@ import net.minecraftforge.registries.{DeferredRegister, RegistryObject}
 object Recipe {
   val RECIPE: DeferredRegister[RecipeSerializer[_]] = DeferredRegister.create(Registry.RECIPE_SERIALIZER_REGISTRY, BookBook.MODID)
 
-  val WATERPROOFED_BOOK: RegistryObject[_] = RECIPE.register("waterproofed_book", () => WaterproofedBookRecipe.getSerializer)
+  val WATERPROOFED_BOOK: RegistryObject[RecipeSerializer[_]] = RECIPE.register("waterproofed_book", () => new SimpleRecipeSerializer[WaterproofedBookRecipe](new WaterproofedBookRecipe(_)))
   def registry(eventBus: IEventBus): Unit = {
     RECIPE.register(eventBus)
   }
