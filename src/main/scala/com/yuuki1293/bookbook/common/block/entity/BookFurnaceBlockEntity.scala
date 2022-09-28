dbc953608ce92ab223cfb3e0999836985366be23
yuuki1293 <yuuki1293@outlook.jp>
2022-09-28 11:43:42 +0900

    fix: type mismatch

--- src/main/scala/com/yuuki1293/bookbook/common/block/entity/BookFurnaceBlockEntity.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/block/entity/BookFurnaceBlockEntity.scala
@@ -6,6 +6,7 @@ import net.minecraft.core.BlockPos
 import net.minecraft.network.chat.{Component, TranslatableComponent}
 import net.minecraft.world.entity.player.Inventory
 import net.minecraft.world.inventory.AbstractContainerMenu
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.crafting.RecipeType
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity
 import net.minecraft.world.level.block.state.BlockState
@@ -14,6 +15,8 @@ class BookFurnaceBlockEntity(worldPosition: BlockPos, blockState: BlockState)
   extends AbstractFurnaceBlockEntity(BlockEntities.BOOK_FURNACE.get(), worldPosition, blockState, RecipeType.SMELTING) {
 
   override protected def getDefaultName: Component = new TranslatableComponent("container.book_furnace")
+
+  override def getBurnDuration(pFuel: ItemStack): Int = super.getBurnDuration(pFuel) / 2
 
   override protected def createMenu(pContainerId: Int, pPlayer: Inventory): AbstractContainerMenu = {
     new BookFurnaceMenu(pContainerId, pPlayer, this, this.dataAccess)
