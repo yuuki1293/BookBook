75b256a802911c6bfb3b22e1af7d679f85a1883d
yuuki1293 <yuuki1293@outlook.jp>
2022-09-29 13:37:40 +0900

    update: FuelSlot

--- src/main/scala/com/yuuki1293/bookbook/common/inventory/FuelSlot.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/inventory/FuelSlot.scala
@@ -1,5 +1,11 @@
 package com.yuuki1293.bookbook.common.inventory
 
-class FuelSlot {
+import net.minecraft.world.Container
+import net.minecraft.world.inventory.Slot
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.crafting.RecipeType
+import net.minecraftforge.common.ForgeHooks
 
+class FuelSlot(pContainer: Container, pSlot: Int, pX: Int, pY: Int) extends Slot(pContainer, pSlot, pX, pY) {
+  override def mayPlace(pStack: ItemStack): Boolean = ForgeHooks.getBurnTime(pStack, RecipeType.SMELTING) > 0
 }
