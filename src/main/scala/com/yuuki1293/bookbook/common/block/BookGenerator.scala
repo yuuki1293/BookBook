1cca65cbb715e4054daf6fdabc8841a6d1b2ada4
yuuki1293 <yuuki1293@outlook.jp>
2022-09-29 23:28:34 +0900

    delete: BookGenerator.scala

--- src/main/scala/com/yuuki1293/bookbook/common/block/BookGenerator.scala
+++
@@ -1,8 +0,0 @@
-package com.yuuki1293.bookbook.common.block
-
-import net.minecraft.world.level.block.Block
-import net.minecraft.world.level.block.state.BlockBehaviour
-
-class BookGenerator(properties: BlockBehaviour.Properties) extends Block(properties) {
-
-}
