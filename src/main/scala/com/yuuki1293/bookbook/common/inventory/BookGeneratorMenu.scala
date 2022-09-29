834bc2273c0b11e47312d4f19d491e77b662ddf1
yuuki1293 <yuuki1293@outlook.jp>
2022-09-29 13:58:45 +0900

    update: BookGeneratorMenu

--- src/main/scala/com/yuuki1293/bookbook/common/inventory/BookGeneratorMenu.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/inventory/BookGeneratorMenu.scala
@@ -1,5 +1,45 @@
 package com.yuuki1293.bookbook.common.inventory
 
-class BookGeneratorMenu {
+import com.yuuki1293.bookbook.common.inventory.BookGeneratorMenu.FUEL_SLOT
+import net.minecraft.world.Container
+import net.minecraft.world.entity.player.{Inventory, Player}
+import net.minecraft.world.inventory.AbstractContainerMenu.{checkContainerDataCount, checkContainerSize}
+import net.minecraft.world.inventory.{AbstractContainerMenu, ContainerData, MenuType, Slot}
+import net.minecraft.world.level.Level
 
+class BookGeneratorMenu(pMenuType: MenuType[_], pContainerId: Int, pPlayerInventory: Inventory, pContainer: Container, pData: ContainerData) extends AbstractContainerMenu(pMenuType, pContainerId) {
+  checkContainerSize(pContainer, 1)
+  checkContainerDataCount(pData, 1)
+  private val container: Container = pContainer
+  private val data: ContainerData = pData
+  protected val level: Level = pPlayerInventory.player.level
+  this.addSlot(new FuelSlot(pContainer, FUEL_SLOT, 56, 35))
+
+  for (i <- 0 until 3) {
+    for (j <- 0 until 9) {
+      this.addSlot(new Slot(pPlayerInventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18))
+    }
+  }
+
+  for (k <- 0 until 9) {
+    this.addSlot(new Slot(pPlayerInventory, k, 8 + k * 18, 142))
+  }
+
+  this.addDataSlots(pData)
+
+  override def stillValid(pPlayer: Player): Boolean = this.container.stillValid(pPlayer)
+
+  def getLitProgress(): Int = {
+    var i = this.data.get(1)
+    if (i==0)
+      i=200
+
+    this.data.get(0) * 13 / i
+  }
+
+  def isLit(): Boolean = this.data.get(0) > 0
+}
+
+object BookGeneratorMenu {
+  val FUEL_SLOT = 0
 }
