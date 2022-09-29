551193dccb0ee4ddae3252fffe1ce8ae9d90a6ac
yuuki1293 <yuuki1293@outlook.jp>
2022-09-29 14:59:01 +0900

    update: Blocks

--- src/main/scala/com/yuuki1293/bookbook/common/register/Blocks.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/register/Blocks.scala
@@ -1,7 +1,7 @@
 package com.yuuki1293.bookbook.common.register
 
 import com.yuuki1293.bookbook.common.BookBook
-import com.yuuki1293.bookbook.common.block.{BookFurnaceBlock, BookShelfBlock, DrownedBookShelfBlock}
+import com.yuuki1293.bookbook.common.block.{BookFurnaceBlock, BookGeneratorBlock, BookShelfBlock, DrownedBookShelfBlock}
 import net.minecraft.world.level.block.{Block, SoundType}
 import net.minecraft.world.level.block.state.BlockBehaviour
 import net.minecraft.world.level.block.state.properties.BlockStateProperties
@@ -15,6 +15,7 @@ object Blocks {
   val BOOKSHELF: RegistryObject[BookShelfBlock] = BLOCKS.register("bookshelf", () => new BookShelfBlock(BlockBehaviour.Properties.of(Material.WOOD).strength(1.5F).sound(SoundType.WOOD)))
   val DROWNED_BOOKSHELF: RegistryObject[DrownedBookShelfBlock] = BLOCKS.register("drowned_bookshelf", () => new DrownedBookShelfBlock(BlockBehaviour.Properties.of(Material.WOOD).strength(1.5F).sound(SoundType.WOOD)))
   val BOOK_FURNACE: RegistryObject[BookFurnaceBlock] = BLOCKS.register("book_furnace", () => new BookFurnaceBlock(BlockBehaviour.Properties.of(Material.WOOD).requiresCorrectToolForDrops().strength(3.5F).sound(SoundType.WOOD).lightLevel(p => if (p.getValue(BlockStateProperties.LIT)) 13 else 0 )))
+  val BOOK_GENERATOR: RegistryObject[BookGeneratorBlock] = BLOCKS.register("book_generator", ()=>new BookGeneratorBlock(BlockBehaviour.Properties.of(Material.WOOD).requiresCorrectToolForDrops().strength(3.5F).sound(SoundType.WOOD).lightLevel(p => if (p.getValue(BlockStateProperties.LIT)) 13 else 0)))
 
   def registry(): Unit = {
     BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus)
