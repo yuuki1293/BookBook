bd88aaf74611f30a9b23eb6d2960394a3c581865
yuuki1293 <yuuki1293@outlook.jp>
2022-09-30 09:05:07 +0900

    update: BookGeneratorBlock

--- src/main/scala/com/yuuki1293/bookbook/common/block/BookGeneratorBlock.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/block/BookGeneratorBlock.scala
@@ -1,8 +1,24 @@
 package com.yuuki1293.bookbook.common.block
 
-import net.minecraft.world.level.block.Block
-import net.minecraft.world.level.block.state.BlockBehaviour
+import com.yuuki1293.bookbook.common.block.entity.BookGeneratorBlockEntity
+import net.minecraft.core.BlockPos
+import net.minecraft.world.{InteractionHand, InteractionResult}
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.entity.{BlockEntity, BlockEntityTicker, BlockEntityType}
+import net.minecraft.world.level.block.{Block, EntityBlock}
+import net.minecraft.world.level.block.state.{BlockBehaviour, BlockState}
+import net.minecraft.world.phys.BlockHitResult
 
-class BookGeneratorBlock(properties: BlockBehaviour.Properties) extends Block(properties) {
+class BookGeneratorBlock(properties: BlockBehaviour.Properties) extends Block(properties) with EntityBlock {
+  override def getTicker[T <: BlockEntity](pLevel: Level, pState: BlockState, pBlockEntityType: BlockEntityType[T]): BlockEntityTicker[T] = {
+    if(pLevel.isClientSide)
+      null
+    else
+      (level0, pos, state0, blockEntity) => blockEntity.asInstanceOf[BookGeneratorBlockEntity].tick()
+  }
 
+  override def newBlockEntity(pPos: BlockPos, pState: BlockState): BlockEntity = {
+    new BookGeneratorBlockEntity(pPos, pState)
+  }
 }
