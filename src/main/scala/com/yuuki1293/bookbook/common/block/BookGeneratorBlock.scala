a1336082c1e92c17ddf787fdd822c6acad1cec42
yuuki1293 <yuuki1293@outlook.jp>
2022-10-01 22:54:27 +0900

    fix: errors

--- src/main/scala/com/yuuki1293/bookbook/common/block/BookGeneratorBlock.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/block/BookGeneratorBlock.scala
@@ -4,10 +4,13 @@ import com.yuuki1293.bookbook.common.block.entity.BookGeneratorBlockEntity
 import net.minecraft.core.BlockPos
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.entity.{BlockEntity, BlockEntityTicker, BlockEntityType}
-import net.minecraft.world.level.block.state.{BlockBehaviour, BlockState}
-import net.minecraft.world.level.block.{Block, EntityBlock}
+import net.minecraft.world.level.block.state.properties.{BlockStateProperties, BooleanProperty, DirectionProperty}
+import net.minecraft.world.level.block.state.{BlockBehaviour, BlockState, StateDefinition}
+import net.minecraft.world.level.block.{Block, EntityBlock, HorizontalDirectionalBlock}
 
 class BookGeneratorBlock(properties: BlockBehaviour.Properties) extends Block(properties) with EntityBlock {
+  val FACING: DirectionProperty = HorizontalDirectionalBlock.FACING
+  val LIT: BooleanProperty = BlockStateProperties.LIT
   override def getTicker[T <: BlockEntity](pLevel: Level, pState: BlockState, pBlockEntityType: BlockEntityType[T]): BlockEntityTicker[T] = {
     if(pLevel.isClientSide)
       null
@@ -17,5 +20,10 @@ class BookGeneratorBlock(properties: BlockBehaviour.Properties) extends Block(pr
 
   override def newBlockEntity(pPos: BlockPos, pState: BlockState): BlockEntity = {
     new BookGeneratorBlockEntity(pPos, pState)
+  }
+
+  override def createBlockStateDefinition(pBuilder: StateDefinition.Builder[Block, BlockState]): Unit = {
+    super.createBlockStateDefinition(pBuilder)
+    pBuilder.add(FACING, LIT)
   }
 }
