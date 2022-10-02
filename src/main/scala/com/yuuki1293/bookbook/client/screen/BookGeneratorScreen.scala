71544323c0b2453150de9b3ca96fa1511b39d400
yuuki1293 <yuuki1293@outlook.jp>
2022-10-03 00:04:25 +0900

    textures/gui/container/book_generator.png

--- src/main/scala/com/yuuki1293/bookbook/client/screen/BookGeneratorScreen.scala
+++ src/main/scala/com/yuuki1293/bookbook/client/screen/BookGeneratorScreen.scala
@@ -40,5 +40,5 @@ class BookGeneratorScreen(pMenu: BookGeneratorMenu, pPlayerInventory: Inventory,
 }
 
 object BookGeneratorScreen {
-  private val TEXTURE = new ResourceLocation(BookBook.MODID, "textures/gui/book_generator.png")
+  private val TEXTURE = new ResourceLocation(BookBook.MODID, "textures/gui/container/book_generator.png")
 }
