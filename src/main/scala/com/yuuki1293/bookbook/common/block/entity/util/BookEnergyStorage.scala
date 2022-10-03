15057cf29249c3af5c97f146bf849b4ede160198
yuuki1293 <yuuki1293@outlook.jp>
2022-10-03 10:53:49 +0900

    change properties name

--- src/main/scala/com/yuuki1293/bookbook/common/block/entity/util/BookEnergyStorage.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/block/entity/util/BookEnergyStorage.scala
@@ -3,12 +3,14 @@ package com.yuuki1293.bookbook.common.block.entity.util
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraftforge.energy.EnergyStorage
 
-class BookEnergyStorage(blockEntity: BlockEntity, capacity: Int, maxReceive: Int, maxExtract: Int, pEnergy: Int) extends EnergyStorage(capacity, maxReceive, maxExtract, pEnergy) {
+class BookEnergyStorage(pBlockEntity: BlockEntity, pCapacity: Int, pMaxReceive: Int, pMaxExtract: Int, pEnergy: Int) extends EnergyStorage(pCapacity, pMaxReceive, pMaxExtract, pEnergy) {
   def this(blockEntity: BlockEntity, capacity: Int) = this(blockEntity, capacity, capacity, capacity, 0)
 
   def this(blockEntity: BlockEntity, capacity: Int, maxTransfer: Int) = this(blockEntity, capacity, maxTransfer, maxTransfer, 0)
 
   def this(blockEntity: BlockEntity, capacity: Int, maxReceive: Int, maxExtract: Int) = this(blockEntity, capacity, maxReceive, maxExtract, 0)
+
+  val blockEntity: BlockEntity = pBlockEntity
 
   override def extractEnergy(maxExtract: Int, simulate: Boolean): Int = {
     this.blockEntity.setChanged()
