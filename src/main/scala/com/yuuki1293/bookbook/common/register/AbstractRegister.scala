7e9ea2f6b1ebbe256276ee4847a38d38ea38400c
yuuki1293 <yuuki1293@outlook.jp>
2022-10-06 09:03:09 +0900

    feat: add AbstractRegister

---
+++ src/main/scala/com/yuuki1293/bookbook/common/register/AbstractRegister.scala
@@ -0,0 +1,12 @@
+package com.yuuki1293.bookbook.common.register
+
+import net.minecraftforge.eventbus.api.IEventBus
+import net.minecraftforge.registries.{DeferredRegister, RegistryObject}
+
+import java.util.function.Supplier
+
+trait AbstractRegister[T] {
+  protected val REGISTER: DeferredRegister[T]
+
+  def registry(implicit eventBus: IEventBus): Unit
+}
