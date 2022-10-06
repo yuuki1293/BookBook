57683589a8903b43f511259222163321564848aa
yuuki1293 <yuuki1293@outlook.jp>
2022-10-06 09:10:58 +0900

    refactor: delete DrownedBookItem

--- src/main/scala/com/yuuki1293/bookbook/common/item/DrownedBookItem.scala
+++
@@ -1,7 +0,0 @@
-package com.yuuki1293.bookbook.common.item
-
-import net.minecraft.world.item.Item
-import net.minecraft.world.item.Item.Properties
-
-class DrownedBookItem(properties: Properties) extends Item(properties) {
-}
