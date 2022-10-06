fb6e090c552920df2c8e8f9f772b17e0573731d1
yuuki1293 <yuuki1293@outlook.jp>
2022-10-06 19:22:49 +0900

    refactor: remove this

--- src/main/scala/com/yuuki1293/bookbook/common/inventory/BookGeneratorMenu.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/inventory/BookGeneratorMenu.scala
@@ -17,42 +17,42 @@ class BookGeneratorMenu(pMenuType: MenuType[_], pContainerId: Int, pPlayerInvent
   private val container: Container = pContainer
   val data: ContainerData = pData
   protected val level: Level = pPlayerInventory.player.level
-  this.addSlot(new FuelSlot(pContainer, SLOT_FUEL, 80, 35))
+  addSlot(new FuelSlot(pContainer, SLOT_FUEL, 80, 35))
 
   for (i <- 0 until 3) {
     for (j <- 0 until 9) {
-      this.addSlot(new Slot(pPlayerInventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18))
+      addSlot(new Slot(pPlayerInventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18))
     }
   }
 
   for (k <- 0 until 9) {
-    this.addSlot(new Slot(pPlayerInventory, k, 8 + k * 18, 142))
+    addSlot(new Slot(pPlayerInventory, k, 8 + k * 18, 142))
   }
 
-  this.addDataSlots(pData)
+  addDataSlots(pData)
 
   def this(pContainerId: Int, pPlayerInventory: Inventory) = this(MenuTypes.BOOK_GENERATOR.get(), pContainerId, pPlayerInventory, new SimpleContainer(1), new SimpleContainerData(4))
 
   override def quickMoveStack(pPlayer: Player, pIndex: Int): ItemStack = {
     var itemStack = ItemStack.EMPTY
-    val slot = this.slots.get(pIndex)
+    val slot = slots.get(pIndex)
     if (slot != null && slot.hasItem) {
       val itemStack1 = slot.getItem
       itemStack = itemStack1.copy
       if (pIndex == 0) {
-        if (!this.moveItemStackTo(itemStack1, 1, 37, true))
+        if (!moveItemStackTo(itemStack1, 1, 37, true))
           return ItemStack.EMPTY
         slot.onQuickCraft(itemStack1, itemStack)
       }
-      else if (this.moveItemStackTo(itemStack1, 0, 1, false)) {
+      else if (moveItemStackTo(itemStack1, 0, 1, false)) {
         return ItemStack.EMPTY
       }
       else if (pIndex >= 1 && pIndex < 28) {
-        if (!this.moveItemStackTo(itemStack1, 28, 37, false))
+        if (!moveItemStackTo(itemStack1, 28, 37, false))
           return ItemStack.EMPTY
-        else if (!this.moveItemStackTo(itemStack1, 1, 28, false))
+        else if (!moveItemStackTo(itemStack1, 1, 28, false))
           return ItemStack.EMPTY
-        else if (!this.moveItemStackTo(itemStack1, 1, 37, false))
+        else if (!moveItemStackTo(itemStack1, 1, 37, false))
           return ItemStack.EMPTY
       }
 
@@ -68,7 +68,7 @@ class BookGeneratorMenu(pMenuType: MenuType[_], pContainerId: Int, pPlayerInvent
     itemStack
   }
 
-  override def stillValid(pPlayer: Player): Boolean = this.container.stillValid(pPlayer)
+  override def stillValid(pPlayer: Player): Boolean = container.stillValid(pPlayer)
 
   def isFuel(stack: ItemStack): Boolean = ForgeHooks.getBurnTime(stack, RecipeType.SMELTING) > 0
 
@@ -79,14 +79,14 @@ class BookGeneratorMenu(pMenuType: MenuType[_], pContainerId: Int, pPlayerInvent
    * @return BurnTime * 13 / BurnDuration
    */
   def getBurnProgress: Int = {
-    var i = this.data.get(DATA_BURN_DURATION)
+    var i = data.get(DATA_BURN_DURATION)
     if (i == 0)
       i = 200
 
-    this.data.get(DATA_BURN_TIME) * 13 / i
+    data.get(DATA_BURN_TIME) * 13 / i
   }
 
-  def isBurn: Boolean = this.data.get(0) > 0
+  def isBurn: Boolean = data.get(0) > 0
 
   /**
    * 0 - Energy is empty<br>
@@ -95,8 +95,8 @@ class BookGeneratorMenu(pMenuType: MenuType[_], pContainerId: Int, pPlayerInvent
    * @return The percentage of energy as a percentage of 100
    */
   def getEnergyProportion: Int = {
-    val energyStored = this.data.get(DATA_ENERGY_STORED)
-    val maxEnergy = this.data.get(DATA_MAX_ENERGY)
+    val energyStored = data.get(DATA_ENERGY_STORED)
+    val maxEnergy = data.get(DATA_MAX_ENERGY)
 
     if (maxEnergy == 0)
       return 0
