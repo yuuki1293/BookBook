b1bec35a27c46deb3442fe18e66630d5e13be82d
yuuki1293 <yuuki1293@outlook.jp>
2022-10-06 09:06:10 +0900

    fix: AbstractRegister.registry make not abstract

--- src/main/scala/com/yuuki1293/bookbook/common/register/AbstractRegister.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/register/AbstractRegister.scala
@@ -1,12 +1,10 @@
 package com.yuuki1293.bookbook.common.register
 
 import net.minecraftforge.eventbus.api.IEventBus
-import net.minecraftforge.registries.{DeferredRegister, RegistryObject}
-
-import java.util.function.Supplier
+import net.minecraftforge.registries.DeferredRegister
 
 trait AbstractRegister[T] {
   protected val REGISTER: DeferredRegister[T]
 
-  def registry(implicit eventBus: IEventBus): Unit
+  def registry(implicit eventBus: IEventBus): Unit = REGISTER.register(eventBus)
 }
