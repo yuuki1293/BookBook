36dcbaab16226d906985d3834ee031c8c01f83ae
yuuki1293 <yuuki1293@outlook.jp>
2022-10-06 09:12:37 +0900

    refactor: delete DrownedBookShelfBlock

--- src/main/scala/com/yuuki1293/bookbook/common/register/Blocks.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/register/Blocks.scala
@@ -1,7 +1,7 @@
 package com.yuuki1293.bookbook.common.register
 
 import com.yuuki1293.bookbook.common.BookBook
-import com.yuuki1293.bookbook.common.block.{BookFurnaceBlock, BookGeneratorBlock, BookShelfBlock, DrownedBookShelfBlock}
+import com.yuuki1293.bookbook.common.block.{BookFurnaceBlock, BookGeneratorBlock, BookShelfBlock}
 import net.minecraft.world.level.block.state.BlockBehaviour
 import net.minecraft.world.level.block.state.properties.BlockStateProperties
 import net.minecraft.world.level.block.{Block, SoundType}
@@ -14,7 +14,7 @@ object Blocks extends AbstractRegister[Block] {
   val REGISTER: DeferredRegister[Block] = DeferredRegister.create(ForgeRegistries.BLOCKS, BookBook.MODID)
 
   val BOOKSHELF: RegistryObject[BookShelfBlock] = REGISTER.register("bookshelf", () => new BookShelfBlock(default()))
-  val DROWNED_BOOKSHELF: RegistryObject[DrownedBookShelfBlock] = REGISTER.register("drowned_bookshelf", () => new DrownedBookShelfBlock(default()))
+  val DROWNED_BOOKSHELF: RegistryObject[Block] = REGISTER.register("drowned_bookshelf", () => new Block(default()))
   val BOOK_FURNACE: RegistryObject[BookFurnaceBlock] = REGISTER.register("book_furnace", () => new BookFurnaceBlock(default().requiresCorrectToolForDrops().strength(3.5F).lightLevel(p => if (p.getValue(BlockStateProperties.LIT)) 13 else 0)))
   val BOOK_GENERATOR: RegistryObject[BookGeneratorBlock] = REGISTER.register("book_generator", () => new BookGeneratorBlock(default().requiresCorrectToolForDrops().strength(3.5F).lightLevel(p => if (p.getValue(BlockStateProperties.LIT)) 13 else 0)))
 }
