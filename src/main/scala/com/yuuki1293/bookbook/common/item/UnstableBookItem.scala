ca751f7ca2395684aff32b0d5c5f46a6d6db2fbd
yuuki1293 <yuuki1293@outlook.jp>
2022-10-07 10:30:42 +0900

    feat: add UnstableBookItem

--- src/main/scala/com/yuuki1293/bookbook/common/item/UnstableBookItem.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/item/UnstableBookItem.scala
@@ -1,6 +1,21 @@
 package com.yuuki1293.bookbook.common.item
 
+import net.minecraft.world.damagesource.DamageSource
+import net.minecraft.world.entity.Entity
 import net.minecraft.world.item.Item
+import net.minecraft.world.level.Explosion
 
 class UnstableBookItem(pProperties: Item.Properties) extends Item(pProperties) {
+}
+
+object UnstableBookItem {
+  /**
+   * Cause unstable ingot explosion<br>
+   * No checks are made
+   * @param entity Player
+   */
+  def explosion(entity: Entity): Unit = {
+    val pos = entity.position()
+    entity.level.explode(entity, DamageSource.badRespawnPointExplosion(), null, pos.x, pos.y, pos.z, 5.0F, true, Explosion.BlockInteraction.DESTROY)
+  }
 }
