91e592df330734c03befe1966f52518016cd4369
yuuki1293 <yuuki1293@outlook.jp>
2022-10-08 10:36:36 +0900

    chore: formatting

--- src/main/scala/com/yuuki1293/bookbook/common/BookBook.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/BookBook.scala
@@ -7,12 +7,12 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
 
 @Mod(BookBook.MOD_ID)
 object BookBook {
-    final val MOD_ID = "bookbook"
+  final val MOD_ID = "bookbook"
 
-    implicit private val eventBus: IEventBus = FMLJavaModLoadingContext.get().getModEventBus
+  implicit private val eventBus: IEventBus = FMLJavaModLoadingContext.get().getModEventBus
 
-    Items.registry
-    Blocks.registry
-    BlockEntities.registry
-    MenuTypes.registry
+  Items.registry
+  Blocks.registry
+  BlockEntities.registry
+  MenuTypes.registry
 }
