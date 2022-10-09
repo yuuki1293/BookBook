4bc5ed160949815078f052ed7403222e3f1606b8
yuuki1293 <yuuki1293@outlook.jp>
2022-10-09 20:35:39 +0900

    feat: BookshelfFlameBlock

---
+++ src/main/scala/com/yuuki1293/bookbook/common/block/BookshelfFlameBlock.scala
@@ -0,0 +1,8 @@
+package com.yuuki1293.bookbook.common.block
+
+import net.minecraft.world.level.block.HalfTransparentBlock
+import net.minecraft.world.level.block.state.BlockBehaviour
+
+class BookshelfFlameBlock(pProperties: BlockBehaviour.Properties) extends HalfTransparentBlock(pProperties) {
+
+}
