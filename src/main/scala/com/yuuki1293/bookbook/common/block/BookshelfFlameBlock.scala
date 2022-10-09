16c38f2f0904a716f8b1f7de82224779f630a870
yuuki1293 <yuuki1293@outlook.jp>
2022-10-09 21:39:05 +0900

    feat: delete BookshelfFlameBlock

--- src/main/scala/com/yuuki1293/bookbook/common/block/BookshelfFlameBlock.scala
+++
@@ -1,8 +0,0 @@
-package com.yuuki1293.bookbook.common.block
-
-import net.minecraft.world.level.block.HalfTransparentBlock
-import net.minecraft.world.level.block.state.BlockBehaviour
-
-class BookshelfFlameBlock(pProperties: BlockBehaviour.Properties) extends HalfTransparentBlock(pProperties) {
-
-}
