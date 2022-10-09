98de52ee9b6ddddc489f6311a54d256c5873d0f4
yuuki1293 <yuuki1293@outlook.jp>
2022-10-09 14:29:07 +0900

    feat: add bookshelf_flame block

--- src/main/scala/com/yuuki1293/bookbook/common/register/Blocks.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/register/Blocks.scala
@@ -17,4 +17,5 @@ object Blocks extends AbstractRegister[Block] {
   val DROWNED_BOOKSHELF: RegistryObject[Block] = REGISTER.register("drowned_bookshelf", () => new Block(default()))
   val BOOK_FURNACE: RegistryObject[BookFurnaceBlock] = REGISTER.register("book_furnace", () => new BookFurnaceBlock(default().requiresCorrectToolForDrops().strength(3.5F).lightLevel(p => if (p.getValue(BlockStateProperties.LIT)) 13 else 0)))
   val BOOK_GENERATOR: RegistryObject[BookGeneratorBlock] = REGISTER.register("book_generator", () => new BookGeneratorBlock(default().requiresCorrectToolForDrops().strength(3.5F).lightLevel(p => if (p.getValue(BlockStateProperties.LIT)) 13 else 0)))
+  val BOOKSHELF_FLAME: RegistryObject[Block] = REGISTER.register("bookshelf_flame", () => new Block(default()))
 }
