47f4d765b26920abca5b17baf20ade9f36aa44ca
yuuki1293 <yuuki1293@outlook.jp>
2022-10-10 16:42:16 +0900

    feat: BookCapacitorBlock.getTicker

--- src/main/scala/com/yuuki1293/bookbook/common/block/BookCapacitorBlock.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/block/BookCapacitorBlock.scala
@@ -3,12 +3,20 @@ package com.yuuki1293.bookbook.common.block
 import com.yuuki1293.bookbook.common.block.BookCapacitorBlock._
 import com.yuuki1293.bookbook.common.block.entity.BookCapacitorBlockEntity
 import net.minecraft.core.BlockPos
-import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.entity.{BlockEntity, BlockEntityTicker, BlockEntityType}
 import net.minecraft.world.level.block.state.{BlockBehaviour, BlockState, StateDefinition}
 import net.minecraft.world.level.block.state.properties.DirectionProperty
 import net.minecraft.world.level.block.{Block, EntityBlock, HorizontalDirectionalBlock}
 
 class BookCapacitorBlock(pProperties: BlockBehaviour.Properties) extends Block(pProperties) with EntityBlock {
+  override def getTicker[T <: BlockEntity](pLevel: Level, pState: BlockState, pBlockEntityType: BlockEntityType[T]): BlockEntityTicker[T] = {
+    if (pLevel.isClientSide)
+      null
+    else
+      (_, _, _, blockEntity) => blockEntity.asInstanceOf[BookCapacitorBlockEntity].tick()
+  }
+
   override def newBlockEntity(pPos: BlockPos, pState: BlockState): BlockEntity = {
     new BookCapacitorBlockEntity(pPos, pState)
   }
