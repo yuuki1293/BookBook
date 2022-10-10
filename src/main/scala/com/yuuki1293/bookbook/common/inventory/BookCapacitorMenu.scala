7f8481f93314b6cb724a7cfcafdd74944a7bef21
yuuki1293 <yuuki1293@outlook.jp>
2022-10-10 16:21:40 +0900

    feat: BookCapacitorMenu.quickMoveStack

--- src/main/scala/com/yuuki1293/bookbook/common/inventory/BookCapacitorMenu.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/inventory/BookCapacitorMenu.scala
@@ -2,9 +2,10 @@ package com.yuuki1293.bookbook.common.inventory
 
 import com.yuuki1293.bookbook.common.register.MenuTypes
 import net.minecraft.world.{Container, SimpleContainer}
-import net.minecraft.world.entity.player.Inventory
+import net.minecraft.world.entity.player.{Inventory, Player}
 import net.minecraft.world.inventory.AbstractContainerMenu.{checkContainerDataCount, checkContainerSize}
 import net.minecraft.world.inventory.{AbstractContainerMenu, ContainerData, MenuType, SimpleContainerData, Slot}
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
 
 class BookCapacitorMenu(pMenuType: MenuType[_], pContainerId: Int, pPlayerInventory: Inventory, pContainer: Container, pData: ContainerData)
@@ -30,4 +31,35 @@ class BookCapacitorMenu(pMenuType: MenuType[_], pContainerId: Int, pPlayerInvent
   addDataSlots(pData)
 
   def this(pContainerId: Int, pPlayerInventory: Inventory) = this(MenuTypes.BOOK_CAPACITOR.get(), pContainerId, pPlayerInventory, new SimpleContainer(2), new SimpleContainerData(2))
+
+  override def quickMoveStack(pPlayer: Player, pIndex: Int): ItemStack = {
+    var itemStack = ItemStack.EMPTY
+    val slot = slots.get(pIndex)
+    if (slot != null && slot.hasItem) {
+      val itemStack1 = slot.getItem
+      itemStack = itemStack1.copy()
+      if (pIndex < 2) {
+        if (!moveItemStackTo(itemStack1, 2, 38, true))
+          return ItemStack.EMPTY
+        slot.onQuickCraft(itemStack1, itemStack)
+      }
+      else if (pIndex < 29) {
+        if (!moveItemStackTo(itemStack1, 29, 38, false))
+          return ItemStack.EMPTY
+        else if (!moveItemStackTo(itemStack1, 2, 29, false))
+          return ItemStack.EMPTY
+        else if (!moveItemStackTo(itemStack1, 2, 38, false))
+          return ItemStack.EMPTY
+      }
+
+      if (itemStack1.isEmpty)
+        slot.set(ItemStack.EMPTY)
+      else
+        slot.setChanged()
+      if (itemStack.getCount == itemStack.getCount)
+        return ItemStack.EMPTY
+    }
+
+    itemStack
+  }
 }
