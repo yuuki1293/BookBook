9647f54966d02e0125b812768db5ed9b12bdc153
yuuki1293 <yuuki1293@outlook.jp>
2022-10-13 21:10:59 +0900

    feat: BookCraftingCoreMenu.this

--- src/main/scala/com/yuuki1293/bookbook/common/inventory/BookCraftingCoreMenu.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/inventory/BookCraftingCoreMenu.scala
@@ -1,15 +1,25 @@
 package com.yuuki1293.bookbook.common.inventory
 
-import net.minecraft.world.Container
+import com.yuuki1293.bookbook.common.register.MenuTypes
+import net.minecraft.world.{Container, SimpleContainer}
 import net.minecraft.world.entity.player.{Inventory, Player}
 import net.minecraft.world.inventory.AbstractContainerMenu.{checkContainerDataCount, checkContainerSize}
-import net.minecraft.world.inventory.{ContainerData, MenuType}
+import net.minecraft.world.inventory.{ContainerData, MenuType, SimpleContainerData, Slot}
+import net.minecraft.world.level.Level
 
 class BookCraftingCoreMenu(pMenuType: MenuType[_], pContainerId: Int, pPlayerInventory: Inventory, pContainer: Container, pData: ContainerData)
   extends AbstractPlayerInventoryMenu(pMenuType, pContainerId, pPlayerInventory)
     with EnergyMenu {
   checkContainerSize(pContainer, 1)
   checkContainerDataCount(pData, 4)
+  private val container: Container = pContainer
+  val data: ContainerData = pData
+  protected val level: Level = pPlayerInventory.player.level
+  addSlot(new Slot(pContainer, 0, 80, 35))
+  addPlayerSlot()
+  addDataSlots(pData)
+
+  def this(pContainerId: Int, pPlayerInventory: Inventory) = this(MenuTypes.BOOK_CRAFTING_CORE.get(), pContainerId, pPlayerInventory, new SimpleContainer(1), new SimpleContainerData(4))
 
   override def getEnergyStored: Int = ???
 
