eb682fed2cfd5e257d55f57997ef19d8634f357f
yuuki1293 <yuuki1293@outlook.jp>
2022-10-13 13:53:52 +0900

    feat: register
    BookCraftingCoreBlock
    BookCraftingCoreBlockEntity

--- src/main/scala/com/yuuki1293/bookbook/common/register/BlockEntities.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/register/BlockEntities.scala
@@ -1,7 +1,7 @@
 package com.yuuki1293.bookbook.common.register
 
 import com.yuuki1293.bookbook.common.BookBook
-import com.yuuki1293.bookbook.common.block.entity.{BookCapacitorBlockEntity, BookFurnaceBlockEntity, BookGeneratorBlockEntity}
+import com.yuuki1293.bookbook.common.block.entity.{BookCapacitorBlockEntity, BookCraftingCoreBlockEntity, BookFurnaceBlockEntity, BookGeneratorBlockEntity}
 import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraftforge.registries.{DeferredRegister, ForgeRegistries, RegistryObject}
 
@@ -11,4 +11,5 @@ object BlockEntities extends AbstractRegister[BlockEntityType[_]] {
   val BOOK_FURNACE: RegistryObject[BlockEntityType[BookFurnaceBlockEntity]] = REGISTER.register("book_furnace_block_entity", () => BlockEntityType.Builder.of(new BookFurnaceBlockEntity(_, _), Blocks.BOOK_FURNACE.get()).build(null))
   val BOOK_GENERATOR: RegistryObject[BlockEntityType[BookGeneratorBlockEntity]] = REGISTER.register("book_generator_block_entity", () => BlockEntityType.Builder.of(new BookGeneratorBlockEntity(_, _), Blocks.BOOK_GENERATOR.get()).build(null))
   val BOOK_CAPACITOR: RegistryObject[BlockEntityType[BookCapacitorBlockEntity]] = REGISTER.register("book_capacitor_block_entity", () => BlockEntityType.Builder.of(new BookCapacitorBlockEntity(_, _), Blocks.BOOK_CAPACITOR.get()).build(null))
+  val BOOK_CRAFTING_CORE: RegistryObject[BlockEntityType[BookCraftingCoreBlockEntity]] = REGISTER.register("book_crafting_core_block_entity", () => BlockEntityType.Builder.of(new BookCraftingCoreBlockEntity(_, _), Blocks.BOOK_CRAFTING_CORE.get()).build(null))
 }
