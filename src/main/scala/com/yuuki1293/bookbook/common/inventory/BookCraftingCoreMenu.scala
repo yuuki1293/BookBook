698b105d2ef46601969cf91f2e105d0c28d4be00
yuuki1293 <yuuki1293@outlook.jp>
2022-10-13 21:16:51 +0900

    feat: BookCraftingCoreMenu
    quickMoveStack

--- src/main/scala/com/yuuki1293/bookbook/common/inventory/BookCraftingCoreMenu.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/inventory/BookCraftingCoreMenu.scala
@@ -6,6 +6,7 @@ import net.minecraft.world.{Container, SimpleContainer}
 import net.minecraft.world.entity.player.{Inventory, Player}
 import net.minecraft.world.inventory.AbstractContainerMenu.{checkContainerDataCount, checkContainerSize}
 import net.minecraft.world.inventory.{ContainerData, MenuType, SimpleContainerData, Slot}
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
 
 class BookCraftingCoreMenu(pMenuType: MenuType[_], pContainerId: Int, pPlayerInventory: Inventory, pContainer: Container, pData: ContainerData)
@@ -27,4 +28,37 @@ class BookCraftingCoreMenu(pMenuType: MenuType[_], pContainerId: Int, pPlayerInv
   override def getMaxEnergy: Int = data.get(DATA_MAX_ENERGY)
 
   override def stillValid(pPlayer: Player): Boolean = container.stillValid(pPlayer)
+
+  override def quickMoveStack(pPlayer: Player, pIndex: Int): ItemStack = {
+    var itemStack = ItemStack.EMPTY
+    val slot = slots.get(pIndex)
+    if (slot != null && slot.hasItem) {
+      val itemStack1 = slot.getItem
+      itemStack = itemStack1.copy()
+      if (pIndex < 1) {
+        if (!moveItemStackTo(itemStack1, 1, 37, true))
+          return ItemStack.EMPTY
+        slot.onQuickCraft(itemStack1, itemStack)
+      }
+      else if (pIndex < 37) {
+        if (!moveItemStackTo(itemStack1, 0, 1, false))
+          return ItemStack.EMPTY
+        else if (!moveItemStackTo(itemStack1, 28, 37, false))
+          return ItemStack.EMPTY
+        else if (!moveItemStackTo(itemStack1, 1, 28, false))
+          return ItemStack.EMPTY
+        else if (!moveItemStackTo(itemStack1, 1, 37, false))
+          return ItemStack.EMPTY
+      }
+
+      if (itemStack1.isEmpty)
+        slot.set(ItemStack.EMPTY)
+      else
+        slot.setChanged()
+      if (itemStack.getCount == itemStack.getCount)
+        return ItemStack.EMPTY
+    }
+
+    itemStack
+  }
 }
