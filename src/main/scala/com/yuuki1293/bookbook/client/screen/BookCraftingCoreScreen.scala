9536c6bd26ae8c55e32e663a7341dd3ccc7677a7
yuuki1293 <yuuki1293@outlook.jp>
2022-10-14 22:24:31 +0900

    feat: BookCraftingCoreScreen
    TEXTURE

--- src/main/scala/com/yuuki1293/bookbook/client/screen/BookCraftingCoreScreen.scala
+++ src/main/scala/com/yuuki1293/bookbook/client/screen/BookCraftingCoreScreen.scala
@@ -2,9 +2,10 @@ package com.yuuki1293.bookbook.client.screen
 
 import com.yuuki1293.bookbook.common.inventory.BookCraftingCoreMenu
 import net.minecraft.network.chat.Component
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.entity.player.Inventory
 
 class BookCraftingCoreScreen(pMenu: BookCraftingCoreMenu, pPlayerInventory: Inventory, pTitle: Component)
   extends AbstractEnergyContainerScreen(pMenu, pPlayerInventory, pTitle) {
-
+  override protected val TEXTURE: ResourceLocation = new ResourceLocation("textures/gui/container/book_crafting_core.png")
 }
