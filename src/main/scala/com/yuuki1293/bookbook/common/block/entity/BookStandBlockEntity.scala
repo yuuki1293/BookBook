ce9a317f26f372cf11ac520e4a99fd15b7c46134
yuuki1293 <yuuki1293@outlook.jp>
2022-10-14 18:55:42 +0900

    fix: Changed the inheritance source from BaseContainerBlockEntity to BlockEntity

--- src/main/scala/com/yuuki1293/bookbook/common/block/entity/BookStandBlockEntity.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/block/entity/BookStandBlockEntity.scala
@@ -2,23 +2,16 @@ package com.yuuki1293.bookbook.common.block.entity
 
 import com.yuuki1293.bookbook.common.register.BlockEntities
 import net.minecraft.core.{BlockPos, Direction}
-import net.minecraft.network.chat.{Component, TranslatableComponent}
 import net.minecraft.world.WorldlyContainer
-import net.minecraft.world.entity.player.{Inventory, Player}
-import net.minecraft.world.inventory.AbstractContainerMenu
+import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.level.block.entity.BaseContainerBlockEntity
+import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 
 class BookStandBlockEntity(pPos: BlockPos, pState: BlockState)
-  extends BaseContainerBlockEntity(BlockEntities.BOOK_STAND.get(), pPos, pState) with WorldlyContainer {
+  extends BlockEntity(BlockEntities.BOOK_STAND.get(), pPos, pState) with WorldlyContainer {
   private var item = ItemStack.EMPTY
-
-  override def getDefaultName: Component = new TranslatableComponent("container.bookbook.book_stand")
-
-  override def createMenu(pContainerId: Int, pInventory: Inventory): AbstractContainerMenu = ???
-
-  override def getSlotsForFace(pSide: Direction): Array[Int] = ???
+  override def getSlotsForFace(pSide: Direction): Array[Int] = Array(0)
 
   override def canPlaceItemThroughFace(pIndex: Int, pItemStack: ItemStack, pDirection: Direction): Boolean = ???
 
