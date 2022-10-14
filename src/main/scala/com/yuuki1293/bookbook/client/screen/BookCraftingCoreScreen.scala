e6d024e0901778659c1f4fe59194cf4cb17f21af
yuuki1293 <yuuki1293@outlook.jp>
2022-10-14 22:25:39 +0900

    feat: BookCraftingCoreScreen
    render

--- src/main/scala/com/yuuki1293/bookbook/client/screen/BookCraftingCoreScreen.scala
+++ src/main/scala/com/yuuki1293/bookbook/client/screen/BookCraftingCoreScreen.scala
@@ -1,5 +1,6 @@
 package com.yuuki1293.bookbook.client.screen
 
+import com.mojang.blaze3d.vertex.PoseStack
 import com.yuuki1293.bookbook.common.inventory.BookCraftingCoreMenu
 import net.minecraft.network.chat.Component
 import net.minecraft.resources.ResourceLocation
@@ -8,4 +9,9 @@ import net.minecraft.world.entity.player.Inventory
 class BookCraftingCoreScreen(pMenu: BookCraftingCoreMenu, pPlayerInventory: Inventory, pTitle: Component)
   extends AbstractEnergyContainerScreen(pMenu, pPlayerInventory, pTitle) {
   override protected val TEXTURE: ResourceLocation = new ResourceLocation("textures/gui/container/book_crafting_core.png")
+
+  override def render(pPoseStack: PoseStack, pMouseX: Int, pMouseY: Int, pPartialTick: Float): Unit = {
+    renderBackground(pPoseStack)
+    super.render(pPoseStack, pMouseX, pMouseY, pPartialTick)
+  }
 }
