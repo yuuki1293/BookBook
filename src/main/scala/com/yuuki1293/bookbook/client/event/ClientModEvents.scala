01566ecd81dbb223ea9025cdbe301e332c8ced13
yuuki1293 <yuuki1293@outlook.jp>
2022-10-14 22:29:58 +0900

    feat: register BookCraftingCoreScreen

--- src/main/scala/com/yuuki1293/bookbook/client/event/ClientModEvents.scala
+++ src/main/scala/com/yuuki1293/bookbook/client/event/ClientModEvents.scala
@@ -1,8 +1,8 @@
 package com.yuuki1293.bookbook.client.event
 
-import com.yuuki1293.bookbook.client.screen.{BookCapacitorScreen, BookGeneratorScreen}
+import com.yuuki1293.bookbook.client.screen.{BookCapacitorScreen, BookCraftingCoreScreen, BookGeneratorScreen}
 import com.yuuki1293.bookbook.common.BookBook
-import com.yuuki1293.bookbook.common.inventory.{BookCapacitorMenu, BookGeneratorMenu}
+import com.yuuki1293.bookbook.common.inventory.{BookCapacitorMenu, BookCraftingCoreMenu, BookGeneratorMenu}
 import com.yuuki1293.bookbook.common.register.{Blocks, MenuTypes}
 import net.minecraft.client.gui.screens.MenuScreens
 import net.minecraft.client.renderer.{ItemBlockRenderTypes, RenderType}
@@ -18,6 +18,7 @@ object ClientModEvents {
   def clientSetup(event: FMLClientSetupEvent): Unit = {
     MenuScreens.register(MenuTypes.BOOK_GENERATOR.get(), (menu: BookGeneratorMenu, inv, title) => new BookGeneratorScreen(menu, inv, title))
     MenuScreens.register(MenuTypes.BOOK_CAPACITOR.get(), (menu: BookCapacitorMenu, inv, title) => new BookCapacitorScreen(menu, inv, title))
+    MenuScreens.register(MenuTypes.BOOK_CRAFTING_CORE.get(), (menu: BookCraftingCoreMenu, inv, title) => new BookCraftingCoreScreen(menu, inv, title))
 
     ItemBlockRenderTypes.setRenderLayer(Blocks.BOOKSHELF_FRAME.get(), RenderType.cutout())
   }
