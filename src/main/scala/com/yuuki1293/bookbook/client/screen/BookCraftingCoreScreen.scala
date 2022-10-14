d55f691d9cc4eabe4c947c1517c0d2a2fa15b598
yuuki1293 <yuuki1293@outlook.jp>
2022-10-14 22:23:28 +0900

    feat: add BookCraftingCoreScreen

---
+++ src/main/scala/com/yuuki1293/bookbook/client/screen/BookCraftingCoreScreen.scala
@@ -0,0 +1,10 @@
+package com.yuuki1293.bookbook.client.screen
+
+import com.yuuki1293.bookbook.common.inventory.BookCraftingCoreMenu
+import net.minecraft.network.chat.Component
+import net.minecraft.world.entity.player.Inventory
+
+class BookCraftingCoreScreen(pMenu: BookCraftingCoreMenu, pPlayerInventory: Inventory, pTitle: Component)
+  extends AbstractEnergyContainerScreen(pMenu, pPlayerInventory, pTitle) {
+
+}
