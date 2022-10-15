ed1bc071381cd9405f8e932f8f81ac8f294a3a3d
yuuki1293 <yuuki1293@outlook.jp>
2022-10-15 11:12:57 +0900

    fix: BookCraftingCoreScreen TEXTURE

--- src/main/scala/com/yuuki1293/bookbook/client/screen/BookCraftingCoreScreen.scala
+++ src/main/scala/com/yuuki1293/bookbook/client/screen/BookCraftingCoreScreen.scala
@@ -1,6 +1,7 @@
 package com.yuuki1293.bookbook.client.screen
 
 import com.mojang.blaze3d.vertex.PoseStack
+import com.yuuki1293.bookbook.common.BookBook
 import com.yuuki1293.bookbook.common.inventory.BookCraftingCoreMenu
 import net.minecraft.network.chat.Component
 import net.minecraft.resources.ResourceLocation
@@ -8,7 +9,7 @@ import net.minecraft.world.entity.player.Inventory
 
 class BookCraftingCoreScreen(pMenu: BookCraftingCoreMenu, pPlayerInventory: Inventory, pTitle: Component)
   extends AbstractEnergyContainerScreen(pMenu, pPlayerInventory, pTitle) {
-  override protected val TEXTURE: ResourceLocation = new ResourceLocation("textures/gui/container/book_crafting_core.png")
+  override val TEXTURE: ResourceLocation = new ResourceLocation(BookBook.MOD_ID, "textures/gui/container/book_crafting_core.png")
 
   override def render(pPoseStack: PoseStack, pMouseX: Int, pMouseY: Int, pPartialTick: Float): Unit = {
     renderBackground(pPoseStack)
