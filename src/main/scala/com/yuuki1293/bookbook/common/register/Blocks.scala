f389ac261b6e9e8d62a8ab0c8c813cae1c8b1164
yuuki1293 <yuuki1293@outlook.jp>
2022-10-15 21:07:02 +0900

    fix: book_crafting_core light level to 0. book_stand light level to 7

--- src/main/scala/com/yuuki1293/bookbook/common/register/Blocks.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/register/Blocks.scala
@@ -19,6 +19,6 @@ object Blocks extends AbstractRegister[Block] {
   val BOOK_GENERATOR: RegistryObject[BookGeneratorBlock] = REGISTER.register("book_generator", () => new BookGeneratorBlock(default().requiresCorrectToolForDrops().strength(3.5F).lightLevel(p => if (p.getValue(BlockStateProperties.LIT)) 13 else 0)))
   val BOOKSHELF_FRAME: RegistryObject[Block] = REGISTER.register("bookshelf_frame", () => new Block(default().strength(1.0F).noOcclusion()))
   val BOOK_CAPACITOR: RegistryObject[BookCapacitorBlock] = REGISTER.register("book_capacitor", () => new BookCapacitorBlock(default().requiresCorrectToolForDrops().strength(3.5F)))
-  val BOOK_CRAFTING_CORE: RegistryObject[BookCraftingCoreBlock] = REGISTER.register("book_crafting_core", () => new BookCraftingCoreBlock(default().requiresCorrectToolForDrops().strength(5.0F).lightLevel(_ => 7)))
-  val BOOK_STAND: RegistryObject[BookStandBlock] = REGISTER.register("book_stand", () => new BookStandBlock(default().requiresCorrectToolForDrops().strength(3.0F)))
+  val BOOK_CRAFTING_CORE: RegistryObject[BookCraftingCoreBlock] = REGISTER.register("book_crafting_core", () => new BookCraftingCoreBlock(default().requiresCorrectToolForDrops().strength(5.0F)))
+  val BOOK_STAND: RegistryObject[BookStandBlock] = REGISTER.register("book_stand", () => new BookStandBlock(default().requiresCorrectToolForDrops().strength(3.0F).lightLevel(_ => 7)))
 }
