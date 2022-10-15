3e739503ad42e20ffd9e990ff131543c6708a7f1
yuuki1293 <yuuki1293@outlook.jp>
2022-10-15 18:53:20 +0900

    feat: register BookStandRenderer

--- src/main/scala/com/yuuki1293/bookbook/client/event/ClientModEvents.scala
+++ src/main/scala/com/yuuki1293/bookbook/client/event/ClientModEvents.scala
@@ -1,10 +1,12 @@
 package com.yuuki1293.bookbook.client.event
 
+import com.yuuki1293.bookbook.client.renderer.BookStandRenderer
 import com.yuuki1293.bookbook.client.screen.{BookCapacitorScreen, BookCraftingCoreScreen, BookGeneratorScreen}
 import com.yuuki1293.bookbook.common.BookBook
 import com.yuuki1293.bookbook.common.inventory.{BookCapacitorMenu, BookCraftingCoreMenu, BookGeneratorMenu}
-import com.yuuki1293.bookbook.common.register.{Blocks, MenuTypes}
+import com.yuuki1293.bookbook.common.register.{BlockEntities, Blocks, MenuTypes}
 import net.minecraft.client.gui.screens.MenuScreens
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderers
 import net.minecraft.client.renderer.{ItemBlockRenderTypes, RenderType}
 import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.eventbus.api.SubscribeEvent
@@ -21,5 +23,7 @@ object ClientModEvents {
     MenuScreens.register(MenuTypes.BOOK_CRAFTING_CORE.get(), (menu: BookCraftingCoreMenu, inv, title) => new BookCraftingCoreScreen(menu, inv, title))
 
     ItemBlockRenderTypes.setRenderLayer(Blocks.BOOKSHELF_FRAME.get(), RenderType.cutout())
+
+    BlockEntityRenderers.register(BlockEntities.BOOK_STAND.get(), new BookStandRenderer(_))
   }
 }
