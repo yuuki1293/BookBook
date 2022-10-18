56ea7fed369d4761ea59971442f653926f40ab18
yuuki1293 <yuuki1293@outlook.jp>
2022-10-18 21:12:07 +0900

    feat: add BaseBookEntityBlock

---
+++ src/main/scala/com/yuuki1293/bookbook/common/block/BaseBookEntityBlock.scala
@@ -0,0 +1,8 @@
+package com.yuuki1293.bookbook.common.block
+
+import net.minecraft.world.level.block.BaseEntityBlock
+import net.minecraft.world.level.block.state.BlockBehaviour
+
+abstract class BaseBookEntityBlock(properties: BlockBehaviour.Properties) extends BaseEntityBlock(properties) {
+
+}
