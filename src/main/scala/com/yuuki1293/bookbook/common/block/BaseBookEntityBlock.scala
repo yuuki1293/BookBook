f5fc5739df61dc16a5b72a2adde018b9e0e5c343
yuuki1293 <yuuki1293@outlook.jp>
2022-10-18 21:19:41 +0900

    feat: delete BaseBookEntityBlock

--- src/main/scala/com/yuuki1293/bookbook/common/block/BaseBookEntityBlock.scala
+++
@@ -1,8 +0,0 @@
-package com.yuuki1293.bookbook.common.block
-
-import net.minecraft.world.level.block.BaseEntityBlock
-import net.minecraft.world.level.block.state.BlockBehaviour
-
-abstract class BaseBookEntityBlock(properties: BlockBehaviour.Properties) extends BaseEntityBlock(properties) {
-
-}
