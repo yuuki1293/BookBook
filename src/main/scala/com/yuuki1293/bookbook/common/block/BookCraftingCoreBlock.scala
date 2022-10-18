d9375987815e78f93e7878da192d3d9f5223dc17
yuuki1293 <yuuki1293@outlook.jp>
2022-10-18 21:45:12 +0900

    refactor: BookCraftingCoreBlock extends BaseBookContainerBlock

--- src/main/scala/com/yuuki1293/bookbook/common/block/BookCraftingCoreBlock.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/block/BookCraftingCoreBlock.scala
@@ -13,7 +13,8 @@ import net.minecraft.world.level.block.state.{BlockBehaviour, BlockState}
 import net.minecraft.world.phys.BlockHitResult
 import net.minecraft.world.{Containers, InteractionHand, InteractionResult, MenuProvider}
 
-class BookCraftingCoreBlock(pProperties: BlockBehaviour.Properties) extends BaseEntityBlock(pProperties) {
+class BookCraftingCoreBlock(pProperties: BlockBehaviour.Properties)
+  extends BaseBookContainerBlock[BookCraftingCoreBlockEntity](pProperties) {
   override def newBlockEntity(pPos: BlockPos, pState: BlockState): BlockEntity =
     new BookCraftingCoreBlockEntity(pPos, pState)
 
@@ -26,33 +27,7 @@ class BookCraftingCoreBlock(pProperties: BlockBehaviour.Properties) extends Base
     }
   }
 
-  protected def openContainer(level: Level, pos: BlockPos, player: Player): Unit = {
-    val blockEntity = level.getBlockEntity(pos)
-    if (blockEntity.isInstanceOf[BookCraftingCoreBlockEntity]) {
-      player.openMenu(blockEntity.asInstanceOf[MenuProvider])
-    }
-  }
-
-  override def onRemove(pState: BlockState, pLevel: Level, pPos: BlockPos, pNewState: BlockState, pIsMoving: Boolean): Unit = {
-    if (!pState.is(pNewState.getBlock)) {
-      val blockEntity = pLevel.getBlockEntity(pPos)
-      blockEntity match {
-        case entity: BookCraftingCoreBlockEntity =>
-          if (pLevel.isInstanceOf[ServerLevel]) {
-            Containers.dropContents(pLevel, pPos, entity)
-          }
-      }
-
-      pLevel.updateNeighbourForOutputSignal(pPos, this)
-    }
-    super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving)
-  }
-
   override def getTicker[T <: BlockEntity](pLevel: Level, pState: BlockState, pBlockEntityType: BlockEntityType[T]): BlockEntityTicker[T] = {
     createTickerHelper(pBlockEntityType, BlockEntities.BOOK_CRAFTING_CORE.get(), BookCraftingCoreBlockEntity.tick)
-  }
-
-  override def getRenderShape(pState: BlockState): RenderShape = {
-    RenderShape.MODEL
   }
 }
