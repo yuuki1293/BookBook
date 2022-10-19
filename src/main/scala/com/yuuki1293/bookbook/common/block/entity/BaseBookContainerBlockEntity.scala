014e7ba964d1bc83d66f6afdeea9fcc020256cc7
yuuki1293 <yuuki1293@outlook.jp>
2022-10-19 10:18:01 +0900

    feat: BaseBookContainerBlockEntity
    stillValid

--- src/main/scala/com/yuuki1293/bookbook/common/block/entity/BaseBookContainerBlockEntity.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/block/entity/BaseBookContainerBlockEntity.scala
@@ -1,15 +1,19 @@
 package com.yuuki1293.bookbook.common.block.entity
 
-import net.minecraft.core.NonNullList
-import net.minecraft.nbt.CompoundTag
+import net.minecraft.core.{BlockPos, NonNullList}
+import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.{Container, ContainerHelper, MenuProvider, Nameable, WorldlyContainer}
+import net.minecraft.world.level.Level
+import net.minecraft.world._
 import net.minecraftforge.common.extensions.IForgeBlockEntity
 
 import scala.jdk.CollectionConverters._
 
 trait BaseBookContainerBlockEntity
   extends IForgeBlockEntity with Container with MenuProvider with Nameable with WorldlyContainer {
+  protected var level: Level
+  protected var worldPosition: BlockPos
+
   var items: NonNullList[ItemStack]
 
   override def getContainerSize: Int = items.size()
@@ -33,5 +37,11 @@ trait BaseBookContainerBlockEntity
     if (pStack.getCount > getMaxStackSize) {
       pStack.setCount(getMaxStackSize)
     }
+  }
+
+  override def stillValid(pPlayer: Player): Boolean = {
+    if(level.getBlockEntity(worldPosition) != this)
+      return false
+    pPlayer.distanceToSqr(worldPosition.getX.toDouble + 0.5D, worldPosition.getY.toDouble + 0.5D, worldPosition.getZ.toDouble + 0.5D) <= 64.0D
   }
 }
