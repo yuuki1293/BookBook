d9bfaab7ed617ff89b95c4ba4c1c56dd47a293a3
yuuki1293 <yuuki1293@outlook.jp>
2022-10-19 12:44:16 +0900

    refactor: BookGeneratorBlockEntity extends BaseBookContainerBlockEntity

--- src/main/scala/com/yuuki1293/bookbook/common/block/entity/BookGeneratorBlockEntity.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/block/entity/BookGeneratorBlockEntity.scala
@@ -10,26 +10,22 @@ import net.minecraft.nbt.CompoundTag
 import net.minecraft.network.chat.{Component, TranslatableComponent}
 import net.minecraft.network.protocol.Packet
 import net.minecraft.network.protocol.game.{ClientGamePacketListener, ClientboundBlockEntityDataPacket}
-import net.minecraft.world.entity.player.{Inventory, Player}
+import net.minecraft.world.entity.player.Inventory
 import net.minecraft.world.inventory.{AbstractContainerMenu, ContainerData}
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.crafting.RecipeType
 import net.minecraft.world.level.Level
-import net.minecraft.world.level.block.entity.{BaseContainerBlockEntity, BlockEntityTicker}
+import net.minecraft.world.level.block.entity.BlockEntityTicker
 import net.minecraft.world.level.block.state.BlockState
-import net.minecraft.world.{ContainerHelper, WorldlyContainer}
 import net.minecraftforge.common.ForgeHooks
 import net.minecraftforge.common.capabilities.Capability
 import net.minecraftforge.common.util.LazyOptional
 import net.minecraftforge.energy.CapabilityEnergy
-import net.minecraftforge.items.wrapper.SidedInvWrapper
-import net.minecraftforge.items.{CapabilityItemHandler, IItemHandlerModifiable}
 
 class BookGeneratorBlockEntity(worldPosition: BlockPos, blockState: BlockState)
-  extends BaseContainerBlockEntity(BlockEntities.BOOK_GENERATOR.get(), worldPosition, blockState)
-    with WorldlyContainer {
+  extends BaseBookContainerBlockEntity(BlockEntities.BOOK_GENERATOR.get(), worldPosition, blockState) {
 
-  protected var items: NonNullList[ItemStack] = NonNullList.withSize(1, ItemStack.EMPTY)
+  override var items: NonNullList[ItemStack] = NonNullList.withSize(1, ItemStack.EMPTY)
 
   private val capacity = 5000
   private val maxExtract = 100
@@ -37,7 +33,7 @@ class BookGeneratorBlockEntity(worldPosition: BlockPos, blockState: BlockState)
   private var burnDuration = 0
 
   val energyStorage: BookEnergyStorage = createEnergyStorage
-  private val energy: LazyOptional[BookEnergyStorage] = LazyOptional.of(() => energyStorage)
+  private var energy: LazyOptional[BookEnergyStorage] = LazyOptional.of(() => energyStorage)
   protected val dataAccess: ContainerData = new ContainerData() {
     /**
      * @param pIndex 0 - [[burnTime]]<br> 1 - [[burnDuration]]<br> 2 - [[getEnergy]]<br> 3 - [[getMaxEnergy]]<br> others - [[UnsupportedOperationException]]
@@ -80,54 +76,11 @@ class BookGeneratorBlockEntity(worldPosition: BlockPos, blockState: BlockState)
       ForgeHooks.getBurnTime(stack, RecipeType.SMELTING)
   }
 
-  override def getContainerSize: Int = items.size()
-
-  override def isEmpty: Boolean = {
-    for (i <- 0 until items.size()) {
-      if (!items.get(i).isEmpty)
-        return false
-    }
-    true
-  }
-
-  override def getItem(pSlot: Int): ItemStack = items.get(pSlot)
-
-  override def removeItem(pSlot: Int, pAmount: Int): ItemStack = ContainerHelper.removeItem(items, pSlot, pAmount)
-
-  override def removeItemNoUpdate(pSlot: Int): ItemStack = ContainerHelper.takeItem(items, pSlot)
-
-  override def setItem(pSlot: Int, pStack: ItemStack): Unit = {
-    items.set(pSlot, pStack)
-    if (pStack.getCount > getMaxStackSize) {
-      pStack.setCount(getMaxStackSize)
-    }
-  }
-
-  override def stillValid(pPlayer: Player): Boolean = {
-    if (level.getBlockEntity(worldPosition) != this) {
-      false
-    } else {
-      pPlayer.distanceToSqr(worldPosition.getX.toDouble + 0.5D, worldPosition.getY.toDouble + 0.5D, worldPosition.getZ.toDouble + 0.5D) <= 64.0D
-    }
-  }
-
   override def canPlaceItem(pIndex: Int, pStack: ItemStack): Boolean = ForgeHooks.getBurnTime(pStack, RecipeType.SMELTING) > 0
-
-  override def clearContent(): Unit = items.clear()
-
-  var handlers: Array[LazyOptional[IItemHandlerModifiable]] = SidedInvWrapper.create(this, Direction.UP, Direction.DOWN, Direction.NORTH)
 
   override def getCapability[A](cap: Capability[A], side: Direction): LazyOptional[A] = {
     if (cap == CapabilityEnergy.ENERGY)
       energy.cast()
-    else if (!remove && side != null && cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
-      if (side == Direction.UP)
-        handlers(0).cast()
-      else if (side == Direction.DOWN)
-        handlers(1).cast()
-      else
-        handlers(2).cast()
-    }
     else
       super.getCapability(cap, side)
   }
@@ -141,22 +94,24 @@ class BookGeneratorBlockEntity(worldPosition: BlockPos, blockState: BlockState)
   override def invalidateCaps(): Unit = {
     super.invalidateCaps()
     energy.invalidate()
-    for (x <- handlers.indices)
-      handlers(x).invalidate()
   }
 
   override def reviveCaps(): Unit = {
     super.reviveCaps()
-    handlers = SidedInvWrapper.create(this, Direction.UP, Direction.DOWN, Direction.NORTH)
+    energy = LazyOptional.of(() => energyStorage)
   }
 
   override def load(pTag: CompoundTag): Unit = {
     super.load(pTag)
-    items = NonNullList.withSize(getContainerSize, ItemStack.EMPTY)
-    ContainerHelper.loadAllItems(pTag, items)
     burnTime = pTag.getInt("BurnTime")
     burnDuration = getBurnDuration(items.get(SLOT_FUEL))
     energyStorage.setEnergy(pTag.getInt("Energy"))
+  }
+
+  override def saveAdditional(pTag: CompoundTag): Unit = {
+    super.saveAdditional(pTag)
+    pTag.putInt("BurnTime", burnTime)
+    pTag.putInt("Energy", getEnergy)
   }
 
   def outputEnergy(): Unit = {
@@ -177,13 +132,6 @@ class BookGeneratorBlockEntity(worldPosition: BlockPos, blockState: BlockState)
         }
       }
     }
-  }
-
-  override protected def saveAdditional(pTag: CompoundTag): Unit = {
-    super.saveAdditional(pTag)
-    pTag.putInt("BurnTime", burnTime)
-    pTag.putInt("Energy", getEnergy)
-    ContainerHelper.saveAllItems(pTag, items)
   }
 
   private def createEnergyStorage: BookEnergyStorage = {
