302eadfae75158656e3e9e6e12d290126585a77e
yuuki1293 <yuuki1293@outlook.jp>
2022-10-19 10:21:09 +0900

    feat: BaseBookContainerBlockEntity
    handlers

--- src/main/scala/com/yuuki1293/bookbook/common/block/entity/BaseBookContainerBlockEntity.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/block/entity/BaseBookContainerBlockEntity.scala
@@ -1,11 +1,14 @@
 package com.yuuki1293.bookbook.common.block.entity
 
-import net.minecraft.core.{BlockPos, NonNullList}
+import net.minecraft.core.{BlockPos, Direction, NonNullList}
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
 import net.minecraft.world._
 import net.minecraftforge.common.extensions.IForgeBlockEntity
+import net.minecraftforge.common.util.LazyOptional
+import net.minecraftforge.items.IItemHandlerModifiable
+import net.minecraftforge.items.wrapper.SidedInvWrapper
 
 import scala.jdk.CollectionConverters._
 
@@ -40,10 +43,12 @@ trait BaseBookContainerBlockEntity
   }
 
   override def stillValid(pPlayer: Player): Boolean = {
-    if(level.getBlockEntity(worldPosition) != this)
+    if (level.getBlockEntity(worldPosition) != this)
       return false
     pPlayer.distanceToSqr(worldPosition.getX.toDouble + 0.5D, worldPosition.getY.toDouble + 0.5D, worldPosition.getZ.toDouble + 0.5D) <= 64.0D
   }
 
   override def clearContent(): Unit = items.clear()
+
+  protected var handlers: Array[LazyOptional[IItemHandlerModifiable]] = SidedInvWrapper.create(this, Direction.UP, Direction.DOWN, Direction.NORTH)
 }
