c716afe7afd69245fa34f4bfba00829df415f981
yuuki1293 <yuuki1293@outlook.jp>
2022-10-19 23:57:38 +0900

    feat: BookEnergyStorage
    outputEnergy

--- src/main/scala/com/yuuki1293/bookbook/common/block/entity/util/BookEnergyStorage.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/block/entity/util/BookEnergyStorage.scala
@@ -1,10 +1,18 @@
 package com.yuuki1293.bookbook.common.block.entity.util
 
+import net.minecraft.core.Direction
 import net.minecraft.world.level.block.entity.BlockEntity
-import net.minecraftforge.energy.EnergyStorage
+import net.minecraftforge.energy.{CapabilityEnergy, EnergyStorage}
+import scala.jdk.OptionConverters._
 
-class BookEnergyStorage(pBlockEntity: BlockEntity, pCapacity: Int, pMaxReceive: Int, pMaxExtract: Int, pEnergy: Int) extends EnergyStorage(pCapacity, pMaxReceive, pMaxExtract, pEnergy) {
+class BookEnergyStorage(pBlockEntity: BlockEntity,
+                        pCapacity: Int,
+                        pMaxReceive: Int,
+                        pMaxExtract: Int,
+                        pEnergy: Int, pEject: Array[Direction] = Direction.values())
+  extends EnergyStorage(pCapacity, pMaxReceive, pMaxExtract, pEnergy) {
   val blockEntity: BlockEntity = pBlockEntity
+  val eject: Array[Direction] = pEject
 
   override def extractEnergy(maxExtract: Int, simulate: Boolean): Int = {
     blockEntity.setChanged()
@@ -21,18 +29,44 @@ class BookEnergyStorage(pBlockEntity: BlockEntity, pCapacity: Int, pMaxReceive: 
   }
 
   def increase(inc: Int): Unit = energy = Math.min(capacity, energy + inc)
+
+  def outputEnergy(): Unit = {
+    if(getEnergyStored <= 0 || !canExtract)
+      return
+
+    val level = blockEntity.getLevel
+    val worldPos = blockEntity.getBlockPos
+
+    for (direction <- eject;
+         be <- Option(level.getBlockEntity(worldPos.relative(direction)));
+         storage <- be.getCapability(CapabilityEnergy.ENERGY, direction.getOpposite).resolve().toScala) {
+      val toSend = extractEnergy(maxExtract, simulate = false)
+      val received = storage.receiveEnergy(toSend, false)
+
+      setEnergy(
+        getEnergyStored + toSend - received
+      )
+    }
+  }
 }
 
 object BookEnergyStorage {
-  def apply(blockEntity: BlockEntity, capacity: Int, maxReceive: Int, maxExtract: Int, energy: Int): BookEnergyStorage =
-    new BookEnergyStorage(blockEntity, capacity, maxReceive, maxExtract, energy)
+  def apply(blockEntity: BlockEntity,
+            capacity: Int,
+            maxReceive: Int,
+            maxExtract: Int,
+            energy: Int = 0,
+            eject: Array[Direction] = Direction.values()): BookEnergyStorage =
+    new BookEnergyStorage(blockEntity, capacity, maxReceive, maxExtract, energy, eject)
 
-  def apply(blockEntity: BlockEntity, capacity: Int): BookEnergyStorage =
-    new BookEnergyStorage(blockEntity, capacity, capacity, capacity, 0)
+  def apply(blockEntity: BlockEntity,
+            capacity: Int,
+            eject: Array[Direction] = Direction.values()): BookEnergyStorage =
+    new BookEnergyStorage(blockEntity, capacity, capacity, capacity, 0, eject)
 
-  def apply(blockEntity: BlockEntity, capacity: Int, maxTransfer: Int): BookEnergyStorage =
-    new BookEnergyStorage(blockEntity, capacity, maxTransfer, maxTransfer, 0)
-
-  def apply(blockEntity: BlockEntity, capacity: Int, maxReceive: Int, maxExtract: Int) =
-    new BookEnergyStorage(blockEntity, capacity, maxReceive, maxExtract, 0)
+  def apply(blockEntity: BlockEntity,
+            capacity: Int,
+            maxTransfer: Int,
+            eject: Array[Direction] = Direction.values()): BookEnergyStorage =
+    new BookEnergyStorage(blockEntity, capacity, maxTransfer, maxTransfer, 0, eject)
 }
