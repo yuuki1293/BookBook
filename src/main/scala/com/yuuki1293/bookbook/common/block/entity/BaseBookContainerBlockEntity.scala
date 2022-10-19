fac89a33586e019ba818d25395f78a85299d6040
yuuki1293 <yuuki1293@outlook.jp>
2022-10-19 11:21:48 +0900

    fix: override level, worldPosition

--- src/main/scala/com/yuuki1293/bookbook/common/block/entity/BaseBookContainerBlockEntity.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/block/entity/BaseBookContainerBlockEntity.scala
@@ -2,11 +2,14 @@ package com.yuuki1293.bookbook.common.block.entity
 
 import net.minecraft.core.BlockPos
 import net.minecraft.nbt.CompoundTag
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.entity.{BaseContainerBlockEntity, BlockEntityType}
 import net.minecraft.world.level.block.state.BlockState
 
 abstract class BaseBookContainerBlockEntity(pType: BlockEntityType[_], pWorldPosition: BlockPos, pBlockState: BlockState)
   extends BaseContainerBlockEntity(pType, pWorldPosition, pBlockState) with IBookContainerBlockEntity {
+  override var level: Level = super[BaseContainerBlockEntity].level
+  override var worldPosition: BlockPos = super[BaseContainerBlockEntity].worldPosition
   override def load(pTag: CompoundTag): Unit = {
     super[BaseContainerBlockEntity].load(pTag)
     super[IBookContainerBlockEntity].load(pTag)
