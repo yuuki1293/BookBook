60729d38968b332b9226967be0f77a651fe49d0c
yuuki1293 <yuuki1293@outlook.jp>
2022-10-26 23:56:16 +0900

    feat: register BookCraftingCategory

--- src/main/scala/com/yuuki1293/bookbook/jei/JeiPlugin.scala
+++ src/main/scala/com/yuuki1293/bookbook/jei/JeiPlugin.scala
@@ -2,7 +2,9 @@ package com.yuuki1293.bookbook.jei
 
 import com.yuuki1293.bookbook.common.BookBook
 import com.yuuki1293.bookbook.jei.JeiPlugin.UID
+import com.yuuki1293.bookbook.jei.category.BookCraftingCategory
 import mezz.jei.api.IModPlugin
+import mezz.jei.api.helpers.IGuiHelper
 import mezz.jei.api.registration.{IRecipeCategoryRegistration, IRecipeRegistration}
 import net.minecraft.resources.ResourceLocation
 
@@ -11,6 +13,9 @@ class JeiPlugin extends IModPlugin {
   override def getPluginUid: ResourceLocation = UID
 
   override def registerCategories(registration: IRecipeCategoryRegistration): Unit = {
+    implicit val helper: IGuiHelper = registration.getJeiHelpers.getGuiHelper
+
+    registration.addRecipeCategories(BookCraftingCategory.apply)
   }
 
   override def registerRecipes(registration: IRecipeRegistration): Unit = {
