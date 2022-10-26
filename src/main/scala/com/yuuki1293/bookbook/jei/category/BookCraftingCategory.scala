0094edac6aeb9e7a0da871b22f82ebf9c5c1fe33
yuuki1293 <yuuki1293@outlook.jp>
2022-10-26 21:14:09 +0900

    feat: BookCraftingCategory
    getTitle

--- src/main/scala/com/yuuki1293/bookbook/jei/category/BookCraftingCategory.scala
+++ src/main/scala/com/yuuki1293/bookbook/jei/category/BookCraftingCategory.scala
@@ -3,11 +3,12 @@ package com.yuuki1293.bookbook.jei.category
 import com.yuuki1293.bookbook.common.recipe.BookCraftingRecipe
 import mezz.jei.api.gui.drawable.IDrawable
 import mezz.jei.api.recipe.category.IRecipeCategory
-import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.{Component, TranslatableComponent}
 import net.minecraft.resources.ResourceLocation
 
 class BookCraftingCategory extends IRecipeCategory[BookCraftingRecipe] {
-  override def getTitle: Component = ???
+  override def getTitle: Component =
+    new TranslatableComponent("jei.category.bookbook.book_crafting")
 
   override def getBackground: IDrawable = ???
 
