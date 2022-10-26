516a2b1ad6383e875aabafabdad1504c03cdc9d5
yuuki1293 <yuuki1293@outlook.jp>
2022-10-26 10:17:48 +0900

    IBookContainerBlockEntityは抽象的なものだけにした

--- src/main/scala/com/yuuki1293/bookbook/common/block/entity/IBookContainerBlockEntity.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/block/entity/IBookContainerBlockEntity.scala
@@ -1,56 +1,8 @@
 package com.yuuki1293.bookbook.common.block.entity
 
-import net.minecraft.core.{BlockPos, NonNullList}
-import net.minecraft.nbt.CompoundTag
-import net.minecraft.world._
-import net.minecraft.world.entity.player.Player
+import net.minecraft.core.NonNullList
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.level.Level
-import net.minecraftforge.common.extensions.IForgeBlockEntity
 
-import scala.jdk.CollectionConverters._
-
-trait IBookContainerBlockEntity
-  extends IForgeBlockEntity with Container with WorldlyContainer {
+trait IBookContainerBlockEntity {
   protected var items: NonNullList[ItemStack]
-
-  override def getContainerSize: Int = items.size()
-
-  override def isEmpty: Boolean = {
-    for (item <- items.asScala) {
-      if (!item.isEmpty)
-        return false
-    }
-    true
-  }
-
-  override def getItem(pSlot: Int): ItemStack = items.get(pSlot)
-
-  override def removeItem(pSlot: Int, pAmount: Int): ItemStack = ContainerHelper.removeItem(items, pSlot, pAmount)
-
-  override def removeItemNoUpdate(pSlot: Int): ItemStack = ContainerHelper.takeItem(items, pSlot)
-
-  override def setItem(pSlot: Int, pStack: ItemStack): Unit = {
-    items.set(pSlot, pStack)
-    if (pStack.getCount > getMaxStackSize) {
-      pStack.setCount(getMaxStackSize)
-    }
-  }
-
-  def stillValid(pPlayer: Player, level: Level, worldPosition: BlockPos): Boolean = {
-    if (level.getBlockEntity(worldPosition) != this)
-      return false
-    pPlayer.distanceToSqr(worldPosition.getX.toDouble + 0.5D, worldPosition.getY.toDouble + 0.5D, worldPosition.getZ.toDouble + 0.5D) <= 64.0D
-  }
-
-  override def clearContent(): Unit = items.clear()
-
-  def load(pTag: CompoundTag): Unit = {
-    items = NonNullList.withSize(getContainerSize, ItemStack.EMPTY)
-    ContainerHelper.loadAllItems(pTag, items)
-  }
-
-  protected def saveAdditional(pTag: CompoundTag): Unit = {
-    ContainerHelper.saveAllItems(pTag, items)
-  }
 }
