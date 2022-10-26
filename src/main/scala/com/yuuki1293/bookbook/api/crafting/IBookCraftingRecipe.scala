08c08cc007fa1d8ac7a8ed8aa0043150a05ffb51
yuuki1293 <yuuki1293@outlook.jp>
2022-10-26 23:18:15 +0900

    fix: ItemStack to Ingredient

--- src/main/scala/com/yuuki1293/bookbook/api/crafting/IBookCraftingRecipe.scala
+++ src/main/scala/com/yuuki1293/bookbook/api/crafting/IBookCraftingRecipe.scala
@@ -2,11 +2,12 @@ package com.yuuki1293.bookbook.api.crafting
 
 import net.minecraft.core.NonNullList
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.crafting.Ingredient
 
 trait IBookCraftingRecipe {
   def getPowerCost: Int
 
   def getPowerRate: Int
 
-  def getInputs: NonNullList[ItemStack]
+  def getInputs: NonNullList[Ingredient]
 }
