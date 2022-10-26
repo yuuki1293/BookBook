412ee88d5bdfab326ba93bac8447941731e224d8
yuuki1293 <yuuki1293@outlook.jp>
2022-10-26 23:50:15 +0900

    feat: BookCraftingCategory
    setRecipe

--- src/main/scala/com/yuuki1293/bookbook/jei/category/BookCraftingCategory.scala
+++ src/main/scala/com/yuuki1293/bookbook/jei/category/BookCraftingCategory.scala
@@ -1,19 +1,20 @@
 package com.yuuki1293.bookbook.jei.category
 
+import com.yuuki1293.bookbook.api.crafting.IBookCraftingRecipe
 import com.yuuki1293.bookbook.common.BookBook
-import com.yuuki1293.bookbook.common.recipe.BookCraftingRecipe
 import com.yuuki1293.bookbook.common.register.Blocks
 import com.yuuki1293.bookbook.jei.category.BookCraftingCategory.{TEXTURE, UID}
 import mezz.jei.api.constants.VanillaTypes
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder
 import mezz.jei.api.gui.drawable.IDrawable
 import mezz.jei.api.helpers.IGuiHelper
-import mezz.jei.api.ingredients.IIngredientType
 import mezz.jei.api.recipe.category.IRecipeCategory
+import mezz.jei.api.recipe.{IFocusGroup, RecipeIngredientRole}
 import net.minecraft.network.chat.{Component, TranslatableComponent}
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.item.ItemStack
 
-class BookCraftingCategory(helper: IGuiHelper) extends IRecipeCategory[BookCraftingRecipe] {
+class BookCraftingCategory(helper: IGuiHelper) extends IRecipeCategory[IBookCraftingRecipe] {
   private final val background = helper.createDrawable(TEXTURE, 0, 0, 140, 171)
   private final val icon =
     helper.createDrawableIngredient(
@@ -29,8 +30,16 @@ class BookCraftingCategory(helper: IGuiHelper) extends IRecipeCategory[BookCraft
 
   override def getUid: ResourceLocation = UID
 
-  override def getRecipeClass: Class[_ <: BookCraftingRecipe] = {
-    getClass[BookCraftingRecipe]
+  override def getRecipeClass: Class[_ <: IBookCraftingRecipe] = {
+    getClass[IBookCraftingRecipe]
+  }
+
+  override def setRecipe(builder: IRecipeLayoutBuilder, recipe: IBookCraftingRecipe, focuses: IFocusGroup): Unit = {
+    builder.addSlot(RecipeIngredientRole.INPUT, 76, 149)
+      .addIngredients(recipe.getInputs.get(0))
+
+    builder.addSlot(RecipeIngredientRole.OUTPUT, 76, 46)
+      .addItemStack(recipe.getResultItem)
   }
 }
 
