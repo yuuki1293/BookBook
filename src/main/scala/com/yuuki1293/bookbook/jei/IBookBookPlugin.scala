8464c479071cea1c80cd03a8276924ba3c62f060
yuuki1293 <yuuki1293@outlook.jp>
2022-10-27 13:43:22 +0900

    fix: rename JeiPlugin to IBookBookPlugin

--- src/main/scala/com/yuuki1293/bookbook/jei/IBookBookPlugin.scala
+++ src/main/scala/com/yuuki1293/bookbook/jei/IBookBookPlugin.scala
@@ -1,15 +1,21 @@
 package com.yuuki1293.bookbook.jei
 
 import com.yuuki1293.bookbook.common.BookBook
-import com.yuuki1293.bookbook.jei.JeiPlugin.UID
+import com.yuuki1293.bookbook.common.recipe.BookCraftingRecipe
+import com.yuuki1293.bookbook.common.register.RecipeTypes
+import com.yuuki1293.bookbook.jei.IBookBookPlugin.UID
 import com.yuuki1293.bookbook.jei.category.BookCraftingCategory
 import mezz.jei.api.IModPlugin
+import mezz.jei.api.JeiPlugin
 import mezz.jei.api.helpers.IGuiHelper
+import mezz.jei.api.recipe.RecipeType
 import mezz.jei.api.registration.{IRecipeCategoryRegistration, IRecipeRegistration}
+import net.minecraft.client.Minecraft
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.SimpleContainer
 
 @JeiPlugin
-class JeiPlugin extends IModPlugin {
+class IBookBookPlugin extends IModPlugin {
   override def getPluginUid: ResourceLocation = UID
 
   override def registerCategories(registration: IRecipeCategoryRegistration): Unit = {
@@ -19,9 +25,19 @@ class JeiPlugin extends IModPlugin {
   }
 
   override def registerRecipes(registration: IRecipeRegistration): Unit = {
+    val level = Option(Minecraft.getInstance().level)
+
+    level match {
+      case Some(level) =>
+        val manager = level.getRecipeManager
+        registration.addRecipes(
+          new RecipeType(BookCraftingCategory.UID, classOf[BookCraftingRecipe]),
+          manager.getAllRecipesFor[SimpleContainer, BookCraftingRecipe](RecipeTypes.BOOK_CRAFTING)
+        )
+    }
   }
 }
 
-object JeiPlugin {
+object IBookBookPlugin {
   final val UID: ResourceLocation = new ResourceLocation(BookBook.MOD_ID, "jei_plugin")
 }
