d4feefd1f38a699929bb765c7d60a6bbef029449
yuuki1293 <yuuki1293@outlook.jp>
2022-10-28 09:43:46 +0900

    cats.effectを削除
    RecipeUtil

--- src/main/scala/com/yuuki1293/bookbook/common/util/RecipeUtil.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/util/RecipeUtil.scala
@@ -1,6 +1,5 @@
 package com.yuuki1293.bookbook.common.util
 
-import cats.effect.IO
 import net.minecraft.world.Container
 import net.minecraft.world.item.crafting.Recipe
 
@@ -14,14 +13,12 @@ object RecipeUtil {
    * @param slot            containerのスロット
    * @return 結果が格納できた場合true、失敗した場合falseを返す
    */
-  def assemble[A <: Container, B <: Container](recipe: Recipe[A], recipeContainer: A, container: B, slot: Int): IO[Boolean] = {
+  def assemble[A <: Container, B <: Container](recipe: Recipe[A], recipeContainer: A, container: B, slot: Int): Boolean = {
     val result = recipe.assemble(recipeContainer)
 
     if (ContainerUtil.canPlace(result, container, slot)) {
-      for {
-        done <- ContainerUtil.place(result, container, slot)
-      } yield done
+      ContainerUtil.place(result, container, slot)
     }
-    else IO(false)
+    else false
   }
 }
