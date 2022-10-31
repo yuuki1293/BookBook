e42feb2576a72877a56c51124a64a72c2796481e
yuuki1293 <yuuki1293@outlook.jp>
2022-10-31 23:34:03 +0900

    feat: RecipeSerializers extends AbstractRegister

--- src/main/scala/com/yuuki1293/bookbook/common/register/RecipeSerializers.scala
+++ src/main/scala/com/yuuki1293/bookbook/common/register/RecipeSerializers.scala
@@ -2,28 +2,13 @@ package com.yuuki1293.bookbook.common.register
 
 import com.yuuki1293.bookbook.common.BookBook
 import com.yuuki1293.bookbook.common.recipe.BookCraftingRecipe
-import com.yuuki1293.bookbook.common.register.RecipeSerializers.BOOK_CRAFTING
-import net.minecraft.core.Registry
-import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.item.crafting.RecipeSerializer
-import net.minecraftforge.event.RegistryEvent
-import net.minecraftforge.eventbus.api.SubscribeEvent
-import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.registries.{DeferredRegister, ForgeRegistries, RegistryObject}
 
-@Mod.EventBusSubscriber
-class RecipeSerializers {
-  @SubscribeEvent
-  def onRegisterSerializers(event: RegistryEvent.Register[RecipeSerializer[_]]): Unit = {
-    val registry = event.getRegistry
+object RecipeSerializers extends AbstractRegister[RecipeSerializer[_]] {
+  override protected val REGISTER: DeferredRegister[RecipeSerializer[_]] =
+    DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, BookBook.MOD_ID)
 
-    registry.register(BOOK_CRAFTING.setRegistryName(new ResourceLocation(BookBook.MOD_ID, "book_crafting")))
-
-    Registry.register(Registry.RECIPE_TYPE, new ResourceLocation(BookBook.MOD_ID, "book_crafting"), RecipeTypes.BOOK_CRAFTING)
-  }
-}
-
-object RecipeSerializers {
-  val BOOK_CRAFTING: RecipeSerializer[BookCraftingRecipe] = BookCraftingRecipe.Serializer
-
-  def apply = new RecipeSerializers
+  val BOOK_CRAFTING_SERIALIZER: RegistryObject[RecipeSerializer[BookCraftingRecipe]] =
+    REGISTER.register("book_crafting", () => BookCraftingRecipe.Serializer)
 }
